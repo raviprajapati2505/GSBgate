41497edd79e51fa955f20c8feb8d6c724b713ec7
Sneyers Tom <tom.sneyers@vito.be>
2015-05-13 09:23:10 +0000

    map loading helper function
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@107 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,14 @@ module ApplicationHelper
   def is_active_action(action_name)
     params[:action] == action_name ? "active" : nil
   end
+
+  def use_gmaps_for(filename)
+    content_for(:js) do
+      javascript_include_tag 'https://maps.google.com/maps/api/js?v=3.13&sensor=false&libraries=geometry',
+                             'https://google-maps-utility-library-v3.googlecode.com/svn/tags/markerclustererplus/2.0.14/src/markerclusterer_packed.js',
+                             'gmaps/google',
+                             "maps/#{filename}"
+    end
+  end
+
 end
