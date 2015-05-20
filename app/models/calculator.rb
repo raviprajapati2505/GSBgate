231bcd6a92fcb902d140ba2448ad8e61facd54ef
Sneyers Tom <tom.sneyers@vito.be>
2015-05-20 14:42:04 +0000

    created polymorphic association between Requirement and Calculator/Document
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@155 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,2 +1,3 @@
 class Calculator < ActiveRecord::Base
+  has_many :requirements, as: :reportable
 end
--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -1,4 +1,5 @@
 class Requirement < ActiveRecord::Base
   has_many :scheme_criterion_requirements
   has_many :scheme_criteria, through: :scheme_criterion_requirements
+  belongs_to :reportable, polymorphic: true
 end
