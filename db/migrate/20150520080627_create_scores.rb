121284dcde836f9e7856396ac4a1d0ebeb8df618
Sneyers Tom <tom.sneyers@vito.be>
2015-05-20 09:33:37 +0000

    refactored score model to be associatied with SchemeCriterion + added seed data for operations phase
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@140 cf4ef618-bcd7-8443-a187-13cdf260f84e

---
+++ db/migrate/20150520080627_create_scores.rb
@@ -0,0 +1,11 @@
+class CreateScores < ActiveRecord::Migration
+  def change
+    create_table :scores do |t|
+      t.integer :score
+      t.text :description
+      t.references :criterion, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
