b0d6877c2b5d70eeb598019bc88cc1813aeab6f3
Vangenechten Ive <ive.vangenechten@vito.be>
2015-05-26 14:52:38 +0000

    Added descendant data for scheme_mix_criterion_requirements, requirement_data, field_data and document_data
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@178 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/models/scheme_mix.rb
+++ app/models/scheme_mix.rb
@@ -8,8 +8,18 @@ class SchemeMix < ActiveRecord::Base
   private
     def create_descendant_records
       scheme.scheme_criteria.each do |scheme_criterion|
-        SchemeMixCriterion.create(targeted_score: 0, scheme_mix: self, scheme_criterion: scheme_criterion)
+        scheme_mix_criterion = SchemeMixCriterion.create(targeted_score: 0, scheme_mix: self, scheme_criterion: scheme_criterion)
+
+        scheme_criterion.requirements.each do |requirement|
+          case requirement.reportable_type
+          when 'Calculator'
+            reportable_datum = FieldDatum.create(field_id: requirement.reportable_id)
+            scheme_mix_criterion.requirement_data.create(reportable_data_type: 'FieldDatum', reportable_data_id: reportable_datum.id)
+          when 'Document'
+            reportable_datum = DocumentDatum.create(document_id: requirement.reportable_id)
+            scheme_mix_criterion.requirement_data.create(reportable_data_type: 'DocumentDatum', reportable_data_id: reportable_datum.id)
+          end
+        end
       end
     end
-
-end
+  end
