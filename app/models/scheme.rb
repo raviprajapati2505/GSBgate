60a788f901938ce1b4a9c3b49850c7c306d76041
Daniels Bart <bart.daniels@vito.be>
2015-05-28 15:28:32 +0000

    show progress bars for max attainable and target scores for each criteria
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@188 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/models/scheme.rb
+++ app/models/scheme.rb
@@ -13,6 +13,14 @@ class Scheme < ActiveRecord::Base
     scheme_criteria.for_category(category).sum(:weight)
   end
 
+  def max_attainable_score_for_category(category)
+    scheme_criteria.for_category(category).joins(:scores).maximum(:score)
+  end
+
+  def min_score_for_category(category)
+    scheme_criteria.for_category(category).joins(:scores).minimum(:score)
+  end
+
   # returns the total of all weighted max attainable scores (see scheme_criterion#weighted_max_attainable_score) for the criteria belonging to the specified category
   def weighted_max_attainable_score_for_category(category)
     weighted_max_attainable_score = 0
@@ -20,6 +28,14 @@ class Scheme < ActiveRecord::Base
       weighted_max_attainable_score += scheme_criterion.scores.to_a.max_by(&:score).score * scheme_criterion.weight / 100
     end
     return weighted_max_attainable_score
+  end
+
+  def weighted_min_score_for_category(category)
+    weighted_min_score = 0
+    scheme_criteria.for_category(category).each do |scheme_criterion|
+      weighted_min_score += scheme_criterion.scores.to_a.min_by(&:score).score * scheme_criterion.weight / 100
+    end
+    return weighted_min_score
   end
 
 end
