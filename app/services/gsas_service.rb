ead1e2d3d24c91fde2db998c8a8e901fac0c0239
Dhallé Kristof <kristof.dhalle@vito.be>
2015-06-08 13:52:54 +0000

    GSAS service refactoring.
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@240 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/services/gsas_service.rb
+++ app/services/gsas_service.rb
@@ -19,6 +19,8 @@ class GsasService
   # - score: An integer with the returned calculator score. Only present when status is "success".
   #
   def call_function(function_name, params = {})
+    error = false
+
     # Build the URI path
     path = PATH + URI.encode(function_name) + '/' + URI.encode(params.to_json)
 
@@ -26,30 +28,34 @@ class GsasService
     begin
       uri = URI::HTTP.build({host: HOST, path: path})
     rescue StandardError
-      return {
-          status: STATUS_ERROR,
-          message: "An error occurred when building a GSAS webservice URI with host: '#{HOST}' and path: '#{path}'. Error message: " + $!.to_s
-      }
+      error = true
+      # todo: log this -> "An error occurred when building a GSAS webservice URI with host: '#{HOST}' and path: '#{path}'. Error message: " + $!.to_s
     end
 
     # Execute the request
     begin
       response_json = Net::HTTP.get_response(uri)
     rescue StandardError
-      return {
-          status: STATUS_ERROR,
-          message: "An error occurred when executing the GSAS webservice request with uri: '#{uri}'. Error message: " + $!.to_s
-      }
+      error = true
+      # todo: log this -> "An error occurred when executing the GSAS webservice request with uri: '#{uri}'. Error message: " + $!.to_s
     end
 
-    # Parse JSON response body and return
+    # Parse JSON response body
     begin
-      JSON.parse response_json.body, :symbolize_names => true
+      response_hash = JSON.parse response_json.body, :symbolize_names => true
     rescue StandardError
-      return {
+      error = true
+      # todo: log this -> "An error occurred when trying to parse the JSON response of the GSAS webservice request with uri: '#{uri}'. Error message: " + $!.to_s
+    end
+
+    # Return hash or error
+    if error
+      {
           status: STATUS_ERROR,
-          message: "An error occurred when trying to parse the JSON response of the GSAS webservice request with uri: '#{uri}'. Error message: " + $!.to_s
+          message: 'An error occurred when calculating the score. Please try again later.'
       }
+    else
+      response_hash
     end
   end
 end
