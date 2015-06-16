79378e22ea0e679f85ad2d60d8f48f04eb530728
Dhallé Kristof <kristof.dhalle@vito.be>
2015-06-16 11:13:39 +0000

    Refactoring.
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@294 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/models/scheme_mix.rb
+++ app/models/scheme_mix.rb
@@ -46,7 +46,7 @@ class SchemeMix < ActiveRecord::Base
       # Loop all the criteria of the scheme
       scheme.scheme_criteria.each do |scheme_criterion|
         # Create a SchemeMixCriterion for every criterion
-        scheme_mix_criterion = SchemeMixCriterion.create(targeted_score: scheme_criterion.max_attainable_score, scheme_mix: self, scheme_criterion: scheme_criterion)
+        scheme_mix_criterion = SchemeMixCriterion.create!(targeted_score: scheme_criterion.max_attainable_score, scheme_mix: self, scheme_criterion: scheme_criterion)
 
         # Loop all requirements of the criterion
         scheme_criterion.requirements.each do |requirement|
@@ -62,25 +62,25 @@ class SchemeMix < ActiveRecord::Base
             # If the Requirement has a Calculator
             if requirement.calculator.present?
               # Create a CalculatorDatum
-              calculator_datum = CalculatorDatum.create(calculator_id: requirement.calculator_id)
+              calculator_datum = CalculatorDatum.create!(calculator_id: requirement.calculator_id)
 
               # Create a RequirementDatum
-              scheme_mix_criterion.requirement_data.create(calculator_datum_id: calculator_datum.id, requirement_id: requirement.id)
+              scheme_mix_criterion.requirement_data.create!(calculator_datum_id: calculator_datum.id, requirement_id: requirement.id)
 
               # Loop all fields of the calculator
               requirement.calculator.fields.each do |field|
                 # Create a FieldDatum for each field
-                calculator_datum.field_data.create(field_id: field.id, type: field.datum_type)
+                calculator_datum.field_data.create!(field_id: field.id, type: field.datum_type)
               end
             # If the Requirement doesn't have a Calculator
             else
               # Create a RequirementDatum
-              scheme_mix_criterion.requirement_data.create(requirement_id: requirement.id)
+              scheme_mix_criterion.requirement_data.create!(requirement_id: requirement.id)
             end
           # If RequirementDatum exists
           else
             # Link the SchemeMixCriterium to the existing RequirementDatum
-            scheme_mix_criterion.scheme_mix_criteria_requirement_data.create(requirement_datum_id: requirement_datum.id)
+            scheme_mix_criterion.scheme_mix_criteria_requirement_data.create!(requirement_datum_id: requirement_datum.id)
           end
         end
       end
