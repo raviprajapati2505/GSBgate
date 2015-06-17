6936ff8a0129c089c9660f48012458d47f9b8131
Dhallé Kristof <kristof.dhalle@vito.be>
2015-06-17 12:15:43 +0000

    Move carrierwave cache dir to /tmp.
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@308 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -16,6 +16,10 @@ class DocumentUploader < CarrierWave::Uploader::Base
     "../private/documents/#{model.id}"
   end
 
+  def cache_dir
+    '../tmp/documents'
+  end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
