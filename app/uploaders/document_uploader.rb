c79d85bc63bcc634bb1096dc9667e3a3fffb577e
Dhallé Kristof <kristof.dhalle@vito.be>
2015-06-24 10:01:28 +0000

    Refactoring.
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@330 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -41,11 +41,14 @@ class DocumentUploader < CarrierWave::Uploader::Base
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_white_list
     %w(7z ace ai bmp cab cdr doc docx dwg eml eps gif gz
     indd jpeg jpg mcd mdb pdf png pps pptx psd pub qxd
     rar rtf sea tgz tif ttf txt xls xlsx zip)
+  end
+
+  def extension_white_list_js
+    '.' + self.extension_white_list.join(',.')
   end
 
   # Override the filename of the uploaded files:
