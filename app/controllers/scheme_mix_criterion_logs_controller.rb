26da9f2f96e7bb66a1e21fab4de755fc71cf37b4
Daniels Bart <bart.daniels@vito.be>
2015-07-09 07:44:29 +0000

    styling criterion status log modal box
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@387 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/controllers/scheme_mix_criterion_logs_controller.rb
+++ app/controllers/scheme_mix_criterion_logs_controller.rb
@@ -2,7 +2,7 @@ class SchemeMixCriterionLogsController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @logs = SchemeMixCriterionLog.for_criterion(params[:scheme_mix_criterion_id]).paginate page: params[:page], per_page: 10
+    @logs = SchemeMixCriterionLog.for_criterion(params[:scheme_mix_criterion_id]).paginate page: params[:page], per_page: 6
     @scheme_mix_criterion = SchemeMixCriterion.find(params[:scheme_mix_criterion_id])
   end
 end
