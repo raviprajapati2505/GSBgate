375a3771f216611271817974ae06c16611d8be35
Dhallé Kristof <kristof.dhalle@vito.be>
2015-07-09 13:32:47 +0000

    Added basic notification system. (bis)
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@390 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < AuthenticatedController
   load_and_authorize_resource
 
   def index
-    @notifications = Notification.for_user(current_user).paginate page: params[:page], per_page: 2
+    @notifications = Notification.for_user(current_user).paginate page: params[:page], per_page: 5
     @notifications.each do |notification|
       notification.read = true
       notification.save
