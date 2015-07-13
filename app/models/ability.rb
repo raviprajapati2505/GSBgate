708ae93ea3d54c6161841884c14b092f1e9524ae
Daniels Bart <bart.daniels@vito.be>
2015-07-13 12:50:21 +0000

    added authorization for notification controller
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@398 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -87,7 +87,8 @@ class Ability
       # can :manage, Document, scheme_mix_criteria: {scheme_mix: {certification_path: {project: {project_authorizations: {user_id: user.id, role: ['cgp_project_manager', ProjectAuthorization.roles[:cgp_project_manager]]}}}}}
       # can :create, Document, scheme_mix_criteria_documents: {scheme_mix_criterion: {requirement_data: {user_id: user.id}}}
       # can :download, Document, scheme_mix_criteria: {scheme_mix: {certification_path: {project: {project_authorizations: {user_id: user.id}}}}}
-
+      # Notification controller
+      can :read, Notification
     else
       cannot :manage, :all
     end
