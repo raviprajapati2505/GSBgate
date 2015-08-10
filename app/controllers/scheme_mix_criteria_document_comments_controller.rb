f66d44cf31eb163e7cefb51efd457ee182bfa302
Dhallé Kristof <kristof.dhalle@vito.be>
2015-08-10 09:46:51 +0000

    Notify document owners of new comments.
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@507 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/controllers/scheme_mix_criteria_document_comments_controller.rb
+++ app/controllers/scheme_mix_criteria_document_comments_controller.rb
@@ -1,4 +1,8 @@
 class SchemeMixCriteriaDocumentCommentsController < AuthenticatedController
+  before_action :set_project
+  before_action :set_certification_path
+  before_action :set_scheme_mix
+  before_action :set_scheme_mix_criterion
   before_action :set_scheme_mix_criteria_document
   load_and_authorize_resource
 
@@ -8,6 +12,13 @@ class SchemeMixCriteriaDocumentCommentsController < AuthenticatedController
     comment.scheme_mix_criteria_document = @scheme_mix_criteria_document
 
     if comment.save
+      # Notify the document owner
+      notify(body: 'A comment was added to your document %s.',
+             body_params: [@scheme_mix_criteria_document.document.document_file.file.filename],
+             uri: project_certification_path_scheme_mix_scheme_mix_criterion_scheme_mix_criteria_document_path(@project, @certification_path, @scheme_mix, @scheme_mix_criterion, @scheme_mix_criteria_document) + '#comment-' + comment.id.to_s,
+             user: @scheme_mix_criteria_document.document.user,
+             project: @project)
+
       redirect_to :back, notice: 'The comment was successfully added.'
     else
       redirect_to :back, alert: 'The comment couldn\'t be saved, please try again later.'
@@ -15,6 +26,21 @@ class SchemeMixCriteriaDocumentCommentsController < AuthenticatedController
   end
 
   private
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def set_certification_path
+    @certification_path = CertificationPath.find(params[:certification_path_id])
+  end
+
+  def set_scheme_mix
+    @scheme_mix = SchemeMix.find(params[:scheme_mix_id])
+  end
+
+  def set_scheme_mix_criterion
+    @scheme_mix_criterion = SchemeMixCriterion.find(params[:scheme_mix_criterion_id])
+  end
 
   def set_scheme_mix_criteria_document
     @scheme_mix_criteria_document = SchemeMixCriteriaDocument.find(params[:scheme_mix_criteria_document_id])
