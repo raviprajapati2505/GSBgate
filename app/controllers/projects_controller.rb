f5135d970294bb303294af688927c73b421fe51b
Dhallé Kristof <kristof.dhalle@vito.be>
2015-08-10 07:11:40 +0000

    Project id can only be filled out by a system administrator.
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@500 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -85,9 +85,9 @@ class ProjectsController < AuthenticatedController
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
       if current_user.system_admin?
-        params.require(:project).permit(:code, :name, :description, :address, :location, :country, :construction_year, :latlng, :gross_area, :certified_area, :carpark_area, :project_site_area, :owner_id)
+        params.require(:project).permit(:name, :description, :address, :location, :country, :construction_year, :latlng, :gross_area, :certified_area, :carpark_area, :project_site_area, :owner_id, :code)
       else
-        params.require(:project).permit(:code, :name, :description, :address, :location, :country, :construction_year, :latlng, :gross_area, :certified_area, :carpark_area, :project_site_area)
+        params.require(:project).permit(:name, :description, :address, :location, :country, :construction_year, :latlng, :gross_area, :certified_area, :carpark_area, :project_site_area)
       end
     end
 end
