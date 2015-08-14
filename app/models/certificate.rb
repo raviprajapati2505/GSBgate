fc26bb57d36f041c4b563fcac7a58bd2ef0985d2
Koen Dierckx <koen.dierckx@vito.be>
2015-08-14 10:02:24 +0000

    convenience functions
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@550 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -5,6 +5,22 @@ class Certificate < ActiveRecord::Base
   has_many :certification_paths
   has_many :schemes
 
+  def letter_of_conformance?
+    design_type? and design_stage?
+  end
+
+  def final_design_certificate?
+    design_type? and construction_stage?
+  end
+
+  def construction_certificate?
+    construction_type? and construction_stage?
+  end
+
+  def operations_certificate?
+    operations_type? and operations_stage?
+  end
+
   scope :letter_of_conformance, -> {
     where(certificate_type: Certificate.certificate_types[:design_type], assessment_stage: Certificate.assessment_stages[:design_stage])
   }
