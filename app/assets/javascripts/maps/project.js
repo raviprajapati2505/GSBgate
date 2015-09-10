5c52740c6e471aaac97726e910c65d27dd382d99
Dhallé Kristof <kristof.dhalle@vito.be>
2015-09-10 10:02:57 +0000

    Project latitude/longitude coordinates can now be manually edited.
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@638 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/assets/javascripts/maps/project.js
+++ app/assets/javascripts/maps/project.js
@@ -15,13 +15,17 @@ function initializeProjectMap() {
 
     // Initialize a marker using the coordinates from the form
     projectMarker = gmaps.initializeMarker(projectMap, wkt.components[0].y, wkt.components[0].x, ($('#project_name').length > 0), function() {
-        var updatedWkt = new Wkt.Wkt(projectMarker);
-        $('#project_latlng').val(updatedWkt.write());
+        updateLatLngFields(projectMarker);
     });
 
     // Move the marker when one of the address fields is changed
     $('#project_address, #project_location, #project_country').change(function() {
         moveMarkerToProjectAddress();
+    });
+
+    // Move the marker when one of the lat lng fields is changed
+    $('#lat, #lng').change(function() {
+        moveMarkerToCoordinates();
     });
 }
 
@@ -45,16 +49,38 @@ function moveMarkerToProjectAddress() {
 
     // Geocode the address
     if (allFieldsFilled) {
-        gmaps.geocode(address, function (location) {
-            if (location != null) {
+        gmaps.geocode(address, function (latLng) {
+            if (latLng != null) {
                 // Move the marker to the geocoded location
-                projectMarker.setPosition(location);
-                projectMap.panTo(location);
+                projectMarker.setPosition(latLng);
+                projectMap.panTo(latLng);
 
-                // Update the WKT latlng text field
-                var updatedWkt = new Wkt.Wkt(projectMarker);
-                $('#project_latlng').val(updatedWkt.write());
+                // Update the WKT latlng text fields
+                updateLatLngFields(projectMarker);
             }
         });
     }
+}
+
+function moveMarkerToCoordinates() {
+    latLng = new google.maps.LatLng($('#lat').val(), $('#lng').val());
+
+    // Validate the lat lng fields
+    if (isNaN(latLng.lat()) || isNaN(latLng.lng())) {
+        toastr.error('The Latitude and Longitude fields can only contain numbers.');
+    }
+    else {
+        projectMarker.setPosition(latLng);
+        projectMap.panTo(latLng);
+    }
+
+    // Update the WKT latlng text fields
+    updateLatLngFields(projectMarker);
+}
+
+function updateLatLngFields(projectMarker) {
+    var updatedWkt = new Wkt.Wkt(projectMarker);
+    $('#project_latlng').val(updatedWkt.write());
+    $('#lat').val(updatedWkt.components[0].y);
+    $('#lng').val(updatedWkt.components[0].x);
 }
