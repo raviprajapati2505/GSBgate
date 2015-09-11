159fd10656464a5be2b0d9790c6309f95042e936
Koen Dierckx <koen.dierckx@vito.be>
2015-09-11 06:55:54 +0000

    generator now generates 2 files, to differantiate between parsed data and 'fake'data (i.e. Requirements)
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@641 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-['generated', 'all', Rails.env].each do |seed|
+['generated', 'generated2', 'all', Rails.env].each do |seed|
   seed_file = "#{Rails.root}/db/seeds/#{seed}.rb"
   if File.exists?(seed_file)
     puts "*** Loading #{seed} seed data"
