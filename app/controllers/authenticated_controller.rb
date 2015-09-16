423b269a833ff57c8dc0e2d9bebc9a042cf6d49a
Koen Dierckx <koen.dierckx@vito.be>
2015-09-16 06:10:18 +0000

    added log for access denied
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@652 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -5,6 +5,7 @@ class AuthenticatedController < ApplicationController
 
   check_authorization
   rescue_from CanCan::AccessDenied do |exception|
+    Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
     redirect_to forbidden_error_path
   end
 end
