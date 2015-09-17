e2e8d326a9ac0673d7857d0178a29523c303faba
Dhallé Kristof <kristof.dhalle@vito.be>
2015-09-17 09:12:08 +0000

    Moved state service methods to CertificationPath class.
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@656 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/models/certification_path.rb
+++ app/models/certification_path.rb
@@ -71,4 +71,263 @@ class CertificationPath < AuditableRecord
       return -1
     end
   end
+
+  # returns false if not all conditions are met to advance to the next status
+  # returns one of CertificationPath.statuses otherwise
+  def next_status
+    error = catch(:error) do
+      case status
+        when CertificationPath.statuses[:awaiting_activation]
+          # Only system_admin can change status
+          if can_leave_awaiting_activation_status?
+            return CertificationPath.statuses[:awaiting_submission]
+          end
+        when CertificationPath.statuses[:awaiting_submission]
+          # Only system_admin and project mngr can change status
+          if can_leave_awaiting_submission_status?
+            return CertificationPath.statuses[:awaiting_screening]
+          end
+        when CertificationPath.statuses[:awaiting_screening]
+          # Only system_admin and certifier mngr can change status
+          if can_leave_awaiting_screening_status?
+            return CertificationPath.statuses[:awaiting_submission_after_screening]
+          end
+        when CertificationPath.statuses[:awaiting_submission_after_screening]
+          # Only system_admin and project mngr can change status
+          if can_leave_awaiting_submission_after_screening_status?
+            # The next status depends on the PCR track flag
+            if pcr_track?
+              return CertificationPath.statuses[:awaiting_pcr_payment]
+            else
+              return CertificationPath.statuses[:awaiting_verification]
+            end
+          end
+        when CertificationPath.statuses[:awaiting_pcr_payment]
+          # Only system_admin can change status
+          if can_leave_awaiting_pcr_payment_status?
+            return CertificationPath.statuses[:awaiting_submission_after_pcr]
+          end
+        when CertificationPath.statuses[:awaiting_submission_after_pcr]
+          # Only system_admin, certifier mngr and project mngr can change status
+          if can_leave_awaiting_submission_after_pcr_status?
+            return CertificationPath.statuses[:awaiting_verification]
+          end
+        when CertificationPath.statuses[:awaiting_verification]
+          # Only system_admin and certifier mngr can change status
+          if can_leave_awaiting_verification_status?
+            # The next status depends on the outcome of the verification process
+            # TODO when is a certification path rejected ?
+            return CertificationPath.statuses[:awaiting_approval_or_appeal]
+          end
+        when CertificationPath.statuses[:awaiting_approval_or_appeal]
+          # Only system_admin and project mngr can change status
+          if can_leave_awaiting_approval_or_appeal_status?
+            # The next status depends if appealed for at least 1 criterion
+            # TODO implement appeal
+            return CertificationPath.statuses[:awaiting_management_approvals]
+          end
+        when CertificationPath.statuses[:awaiting_appeal_payment]
+          # Only system_admin can change status
+          if can_leave_awaiting_appeal_payment_status?
+            return CertificationPath.statuses[:awaiting_submission_after_appeal]
+          end
+        when CertificationPath.statuses[:awaiting_submission_after_appeal]
+          # Only system_admin and project mngr can change status
+          if can_leave_awaiting_submission_after_appeal_status?
+            return CertificationPath.statuses[:awaiting_verification_after_appeal]
+          end
+        when CertificationPath.statuses[:awaiting_verification_after_appeal]
+          # Only system_admin and certifier mngr can change status
+          if can_leave_awaiting_verification_after_appeal_status?
+            # The next status depends on the outcome of the verification process
+            # TODO when is a certification path rejected ?
+            return CertificationPath.statuses[:awaiting_approval_after_appeal]
+          end
+        when CertificationPath.statuses[:awaiting_approval_after_appeal]
+          # Only system_admin and project mngr can change status
+          if can_leave_awaiting_approval_after_appeal_status?
+            return CertificationPath.statuses[:awaiting_management_approvals]
+          end
+        when CertificationPath.statuses[:awaiting_management_approvals]
+          # Only GORD mngr and GORD top mngr can change status
+          if can_leave_awaiting_management_approvals_status?
+            return CertificationPath.statuses[:certified]
+          end
+        when CertificationPath.statuses[:certified]
+          # There is no next status
+          throw(:error, 'There is no next status')
+      end
+    end
+    return error
+  end
+
+  def previous_status
+    # only system_admin can use this function
+    # TODO provide code for previous_status
+    raise NotImplementedError
+  end
+
+  private
+
+  # Conditions are
+  #  1) certification path is not expired
+  #  2) certification path payment received
+  #  3) project details are provided
+  #  4) certifier mngr is assigned
+  def can_leave_awaiting_activation_status?
+    # TODO certification path expiry date
+    # certifier mngr is assigned
+    if project.certifier_manager_assigned?
+      return true
+    end
+    throw(:error, 'A certifier manager must be assigned to the project first')
+  end
+
+  # Conditions are
+  #  1) all criteria are completed (= when all linked requirements and submitted scores are provided and no more documents waiting for approval)
+  def can_leave_awaiting_submission_status?
+    scheme_mix_criteria.each do |criterion|
+      # all criteria are completed
+      unless criterion.complete?
+        throw(:error, 'All criteria must have status complete')
+      end
+      # all submitted scores provided
+      if criterion.submitted_score.blank?
+        throw(:error, 'There are submitted scores missing')
+      end
+      # all linked requirements are provided
+      if criterion.has_required_requirements?
+        throw(:error, 'There are still requirements in status \'required\'')
+      end
+      # no more documents waiting for approval
+      if criterion.has_documents_awaiting_approval?
+        throw(:error, 'There are still documents awaiting approval')
+      end
+    end
+    return true
+  end
+
+  # Conditions are
+  #  1) all criteria are screened
+  def can_leave_awaiting_screening_status?
+    return true
+  end
+
+  # Conditions are
+  #  1) all criteria are completed (= when all linked requirements and submitted scores are provided and no more documents waiting for approval)
+  def can_leave_awaiting_submission_after_screening_status?
+    scheme_mix_criteria.each do |criterion|
+      # all criteria are completed
+      unless criterion.complete?
+        throw(:error, 'All criteria must have status complete')
+      end
+      # all submitted scores provided
+      if criterion.submitted_score.blank?
+        throw(:error, 'There are submitted scores missing')
+      end
+      # all linked requirements are provided
+      if criterion.has_required_requirements?
+        throw(:error, 'There are still requirements in status \'required\'')
+      end
+      # no more documents waiting for approval
+      if criterion.has_documents_awaiting_approval?
+        throw(:error, 'There are still documents awaiting approval')
+      end
+    end
+    return true
+  end
+
+  # Conditions are
+  #  1) PCR track payment received
+  def can_leave_awaiting_pcr_payment_status?
+    if pcr_track_allowed?
+      return true
+    end
+    throw(:error, 'The PCR track allowed flag must be set first')
+  end
+
+  # Conditions are
+  #  1) all criteria are reviewed
+  def can_leave_awaiting_submission_after_pcr_status?
+    return true
+  end
+
+  # Conditions are
+  #  1) all criteria are verified (= when all achieved scores are provided)
+  def can_leave_awaiting_verification_status?
+    scheme_mix_criteria.each do |criterion|
+      unless criterion.approved? || criterion.resubmit?
+        throw(:error, 'There are still criteria in status \'complete\'')
+      end
+      if criterion.achieved_score.blank?
+        throw(:error, 'There are achieved scores missing')
+      end
+    end
+    return true
+  end
+
+  # Conditions are
+  #  none
+  def can_leave_awaiting_approval_or_appeal_status?
+    return true
+  end
+
+  # Conditions are
+  #  1) payment received
+  def can_leave_awaiting_appeal_payment_status?
+    return true
+  end
+
+  # Conditions are
+  #  1) all appealed criteria are completed (= when all linked requirements and submitted scores are provided and no more documents waiting for approval)
+  def can_leave_awaiting_submission_after_appeal_status?
+    scheme_mix_criteria.each do |criterion|
+      # all criteria are completed
+      unless criterion.complete?
+        throw(:error, 'All criteria must have status complete')
+      end
+      # all submitted scores provided
+      if criterion.submitted_score.blank?
+        throw(:error, 'There are submitted scores missing')
+      end
+      # all linked requirements are provided
+      if criterion.has_required_requirements?
+        throw(:error, 'There are still requirements in status \'required\'')
+      end
+      # no more documents waiting for approval
+      if criterion.has_documents_awaiting_approval?
+        throw(:error, 'There are still documents awaiting approval')
+      end
+    end
+    return true
+  end
+
+  # Conditions are
+  #  1) all criteria are verified (= when all achieved scores are provided)
+  def can_leave_awaiting_verification_after_appeal_status?
+    scheme_mix_criteria.each do |criterion|
+      unless criterion.approved? || criterion.resubmit?
+        throw(:error, 'There are still criteria in status \'complete\'')
+      end
+      if criterion.achieved_score.blank?
+        throw(:error, 'There are achieved scores missing')
+      end
+    end
+    return true
+  end
+
+  # Conditions are
+  #  none
+  def can_leave_awaiting_approval_after_appeal_status?
+    return true
+  end
+
+  # Conditions are
+  #  1) signed flag set for both GORD mngr and GORD top mngr
+  def can_leave_awaiting_management_approvals_status?
+    if signed_by_mngr? && signed_by_top_mngr?
+      return true
+    end
+    throw(:error, 'Both signed by GORD manager and signed by GORD top manager must be set')
+  end
 end
