30fe8e251619ea600ff1dbe61530193605b440f9
Daniels Bart <bart.daniels@vito.be>
2015-09-28 09:48:33 +0000

    refactoring
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@667 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/assets/javascripts/select_user.js
+++ app/assets/javascripts/select_user.js
@@ -1,38 +1,22 @@
 $(function () {
     var unauthorized_users_list = $('.select2-ajax[data-project-id]');
     if (unauthorized_users_list.length > 0) {
-        unauthorized_users_list.select2({
-            placeholder: "Type the first letters of the email address",
-            width: "100%",
-            ajax: {
-                url: Routes.list_unauthorized_users_path({project_id: $('.select2-ajax').data('project-id')}),
-                dataType: 'json',
-                quietMillis: 250,
-                data: function (term, page) {
-                    return {
-                        q: term,
-                        page: page
-                    };
-                },
-                results: function (data, page) {
-                    var more = (page * 25) < data.total_count;
-                    return {
-                        results: data.items,
-                        more: more
-                    };
-                },
-                cache: false
-            }
-        });
+        element = unauthorized_users_list;
+        url = Routes.list_unauthorized_users_path({project_id: $('.select2-ajax').data('project-id')});
     }
 
     var users_sharing_projects_list = $('.select2-ajax[data-user-id]');
     if (users_sharing_projects_list.length > 0) {
-        users_sharing_projects_list.select2({
+        element = users_sharing_projects_list;
+        url = Routes.list_users_sharing_projects_path({user_id: $('.select2-ajax').data('user-id')});
+    }
+
+    if ((typeof element !== 'undefined') && (typeof url !== 'undefined')) {
+        element.select2({
             placeholder: "Type the first letters of the email address",
             width: "100%",
             ajax: {
-                url: Routes.list_users_sharing_projects_path({user_id: $('.select2-ajax').data('user-id')}),
+                url: url,
                 dataType: 'json',
                 quietMillis: 250,
                 data: function (term, page) {
