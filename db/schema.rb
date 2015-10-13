a2fbea709b684d2da9e21912c4f943208e66df05
Dhallé Kristof <kristof.dhalle@vito.be>
2015-10-13 07:29:03 +0000

    Approving by top and normal management is now done by advancing the certificate status. (bis)
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@753 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008092256) do
+ActiveRecord::Schema.define(version: 20151013071049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,8 +73,6 @@ ActiveRecord::Schema.define(version: 20151008092256) do
     t.datetime "created_at",                                   null: false
     t.datetime "updated_at",                                   null: false
     t.integer  "certificate_id"
-    t.boolean  "signed_by_mngr",               default: false
-    t.boolean  "signed_by_top_mngr",           default: false
     t.boolean  "pcr_track",                    default: false
     t.boolean  "pcr_track_allowed",            default: false
     t.integer  "duration"
