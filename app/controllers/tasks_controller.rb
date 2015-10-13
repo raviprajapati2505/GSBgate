9ff994fda89e5eed9f62ca3bf3b1c684cf86425e
Daniels Bart <bart.daniels@vito.be>
2015-10-13 15:04:58 +0000

    refactoring
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@760 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,17 +5,11 @@ class TasksController < AuthenticatedController
     @page_title = 'Tasks'
     @default_values = {user_id: '', project_id: nil}
 
-    # User filter
-    if params[:user_id].present? and (params[:user_id].to_i > 0)
-      user = User.find(params[:user_id].to_i)
-      @default_values[:user_id] = params[:user_id]
-    end
-
     # Project filter
     if params[:project_id].present? and (params[:project_id].to_i > 0)
       @default_values[:project_id] = params[:project_id]
     end
 
-    @tasks = TaskService::get_tasks(page: params[:page], user: (user.present? ? user : nil), project_id: @default_values[:project_id])
+    @tasks = TaskService::get_tasks(page: params[:page], user: current_user, project_id: @default_values[:project_id])
   end
 end
