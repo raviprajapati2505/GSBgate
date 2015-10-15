df6c3b55254aee44650fccbbf1994609f82caee1
Koen Dierckx <koen.dierckx@vito.be>
2015-10-15 09:50:16 +0000

    refactored document upload for better integration with cancan and strong parameters
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@773 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,26 +2,25 @@ class DocumentsController < AuthenticatedController
   load_and_authorize_resource :project
   load_and_authorize_resource :certification_path, :through => :project
   load_and_authorize_resource :document
-  # cancan authorization will fail during create, as we don't have a link with the rest of our models yet
-  #  todo: have our form pass correctly formatted params, so cancan load resource can create the necessary child models, so we cna pass authorization
+  # cancan authorization fails (related to carrierwave?)
   skip_authorize_resource :document, :only => :create
-  before_action :set_controller_model
+  before_action :set_controller_model, except: [:new, :create]
 
   def create
+    authorize! :create , Document
     respond_to do |format|
       if params.has_key?(:document)
-        # Create the document
-        @document = Document.new(document_file: params[:document]['document_file'], user: current_user)
-
+        # Creates the document and its scheme_mix_criteria_documents
+        @document = Document.new(document_params)
+        # Set the user
+        @document.user = current_user
         # Set the directory where the file will be stored, this is used by the DocumentUploader class
         @document.store_dir = "projects/#{@project.id}/certification_paths/#{@certification_path.id}/documents"
-
+        # Add the user comment to all scheme_mix_criteria_documents
+        @document.scheme_mix_criteria_documents.each do |scheme_mix_criteria_document|
+          scheme_mix_criteria_document.audit_log_user_comment = params[:scheme_mix_criteria_document]['audit_log_user_comment']
+        end
         if @document.save
-          # Create links with the scheme mix criteria
-          params[:document]['scheme_mix_criteria'].each do |scheme_mix_criterion_id|
-            @document.scheme_mix_criteria_documents.create!(scheme_mix_criterion_id: scheme_mix_criterion_id, audit_log_user_comment: params[:scheme_mix_criteria_document]['audit_log_user_comment'])
-          end
-
           format.html { redirect_to :back, notice: 'The document was successfully uploaded.' }
           format.json { render :json => @document }
         else
@@ -41,12 +40,8 @@ class DocumentsController < AuthenticatedController
     @controller_model = @document
   end
 
-  def create_params
-    # TODO: handle custom params when creating a document
-  end
-
   # Never trust parameters from the scary internet, only allow the white list through.
   def document_params
-    params.require(:document).permit(:document_file)
+    params.require(:document).permit(:document_file, scheme_mix_criteria_documents_attributes: [:scheme_mix_criterion_id, :document_id, :status])
   end
 end
--- app/models/document.rb
+++ app/models/document.rb
@@ -7,8 +7,11 @@ class Document < ActiveRecord::Base
   has_many :scheme_mix_criteria_documents
   has_many :scheme_mix_criteria, through: :scheme_mix_criteria_documents
 
+  accepts_nested_attributes_for :scheme_mix_criteria_documents, :allow_destroy => true
+
   mount_uploader :document_file, DocumentUploader
   validates :document_file, file_size: {maximum: MAXIMUM_DOCUMENT_FILE_SIZE.megabytes.to_i }
+  validates :scheme_mix_criteria_documents, :presence => true
 
   def name
     self.document_file.file.filename
