c490240c731076baa91d91fde599c8ed48941a2d
Koen Dierckx <koen.dierckx@vito.be>
2015-10-21 09:41:35 +0000

    data parts of score graph are now optional
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@805 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/assets/javascripts/score_widget.js
+++ app/assets/javascripts/score_widget.js
@@ -1,6 +1,6 @@
 $(function () {
     $('.score-graph').each(function () {
-        score_graph(this, $(this).data('show-legend'),  $(this).data('show-x-axis'), $(this).data('show-values'), $(this).data('width'), $(this).data('height'), $(this).data('min'), $(this).data('max'), $(this).data('maxAttainable'), $(this).data('targeted'), $(this).data('submitted'), $(this).data('achieved'));
+        score_graph(this, $(this).data('show-legend'), $(this).data('show-x-axis'), $(this).data('show-values'), $(this).data('width'), $(this).data('height'), $(this).data('min'), $(this).data('max'), $(this).data('maxAttainable'), $(this).data('targeted'), $(this).data('submitted'), $(this).data('achieved'));
     });
 });
 
@@ -8,63 +8,78 @@ function score_graph($element, showLegend, showXaxis, showValues, width, height,
     if (!showXaxis) {
         height -= 18;
     }
-    var margin = {top: (showXaxis ? 18 : 0), right: (showLegend ? 160 : 35), bottom: 0, left: 35},
-        graph_width = width - margin.left - margin.right,
-        graph_height = height - margin.top - margin.bottom,
-        data = [{class: 'progress-bar-max', value: maxAttainable, name: 'Max. Attainable score'},
-            {class: 'progress-bar-targeted', value: targeted, name: 'Targeted score'},
-            {class: 'progress-bar-submitted', value: submitted, name: 'Submitted score'},
-            {class: 'progress-bar-achieved', value: achieved, name: 'Achieved score'}];
+    var data = [];
+    if(maxAttainable) {
+        data.push({class: 'progress-bar-max', value: maxAttainable, name: 'Max. Attainable score'});
+    }
+    if(targeted) {
+        data.push({class: 'progress-bar-targeted', value: targeted, name: 'Targeted score'});
+    }
+    if(submitted) {
+        data.push({class: 'progress-bar-submitted', value: submitted, name: 'Submitted score'});
+    }
+    if(achieved) {
+        data.push({class: 'progress-bar-achieved', value: achieved, name: 'Achieved score'});
+    }
+    var margin = {
+            top: (showXaxis ? 18 : 0),
+            right: (showLegend ? 160 : 35),
+            bottom: 0,
+            left: 35
+        },
+        graph_width = (width - margin.left - margin.right),
+        graph_height = (height - margin.top - margin.bottom),
+        data = data;
 
     var x = d3.scale.linear()
-                .domain([min, max])
-                .range([0, graph_width]);
+        .domain([min, max])
+        .range([0, graph_width]);
     var y = d3.scale.ordinal()
-                .domain(data.map(function(d) {
-                    return d.class;
-                }))
-                .rangeRoundBands([0, graph_height], .2);
+        .domain(data.map(function (d) {
+            return d.class;
+        }))
+        .rangeRoundBands([0, graph_height], .2);
 
     var xAxis = d3.svg.axis()
-                .scale(x)
-                .orient("top")
-                .ticks(5);
+        .scale(x)
+        .orient("top")
+        .ticks(5);
 
     var svg = d3.select($element)
         .append('svg')
-            .attr('viewBox', '0 0 ' + width + ' ' + height)
-            .attr('preserveAspectRatio', 'none')
+        .attr('viewBox', '0 0 ' + width + ' ' + height)
+        .attr('preserveAspectRatio', 'none')
         .append('g')
-            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
     var tip = d3.tip()
         .attr('class', 'progress-bar-tip')
         .offset([-10, 0])
-        .html(function(d) {
+        .html(function (d) {
             return d.name + ' = ' + d.value;
         });
 
     svg.call(tip);
 
     var bars = svg.selectAll('.bar')
-                    .data(data)
-                .enter().append('g')
-                    .attr('class', 'progress-bar')
-                    .on('mouseover', tip.show)
-                    .on('mouseout', tip.hide);
+        .data(data)
+        .enter().append('g')
+        .attr('class', 'progress-bar')
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
 
     bars.append('rect')
-        .attr('class', function(d) {
+        .attr('class', function (d) {
             //return d.value < 0 ? 'bar negative' : 'bar positive';
             return d.class;
         })
-        .attr('x', function(d) {
+        .attr('x', function (d) {
             return x(Math.min(0, d.value));
         })
-        .attr('y', function(d) {
+        .attr('y', function (d) {
             return y(d.class);
         })
-        .attr('width', function(d) {
+        .attr('width', function (d) {
             return Math.abs(x(d.value) - x(0));
         })
         .attr('height', y.rangeBand());
@@ -95,20 +110,20 @@ function score_graph($element, showLegend, showXaxis, showValues, width, height,
     }
 
     svg.append('g')
-            .attr('class', 'y axis')
+        .attr('class', 'y axis')
         .append('line')
-            .attr('x1', x(0))
-            .attr('x2', x(0))
-            .attr('y2', graph_height);
+        .attr('x1', x(0))
+        .attr('x2', x(0))
+        .attr('y2', graph_height);
 
     if (showLegend == true) {
         var legend = svg.selectAll('.legend')
-                .data(data)
+            .data(data)
             .enter().append('g')
-                .attr('class', 'legend')
-                .attr('transform', function (d, i) {
-                    return 'translate(0,' + i * 10 + ')';
-                });
+            .attr('class', 'legend')
+            .attr('transform', function (d, i) {
+                return 'translate(0,' + i * 10 + ')';
+            });
 
         legend.append('rect')
             .attr('class', function (d) {
