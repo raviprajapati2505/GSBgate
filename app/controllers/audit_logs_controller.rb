b99e1b967f5bd4670af08ac35d013681f40427ec
Daniels Bart <bart.daniels@vito.be>
2015-10-22 10:22:45 +0000

    bug fix : remove filter didn't work anymore for tasks/audit-logs pages
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@817 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -10,81 +10,86 @@ class AuditLogsController < AuthenticatedController
       @projects = current_user.projects
     end
 
-    if params[:button].present?
-      session[:audit] = {'text' => nil, 'user_id' => nil, 'project_id' => nil, 'certification_path_id' => nil, 'date_from' => '', 'time_from' => '0:00', 'date_to' => '', 'time_to' => '0:00', 'only_comments' => false}
-    else
-      session[:audit] ||= {'text' => nil, 'user_id' => nil, 'project_id' => nil, 'certification_path_id' => nil, 'date_from' => '', 'time_from' => '0:00', 'date_to' => '', 'time_to' => '0:00', 'only_comments' => false}
-    end
-
     @audit_logs = AuditLog.for_user_projects(current_user)
     @certification_paths_optionlist = []
 
-    # Text filter
-    if params[:text].present?
-      session[:audit]['text'] = params[:text]
-    end
-    if session[:audit]['text'].present?
-      @audit_logs = @audit_logs.where('(system_message ILIKE ?) OR (user_comment ILIKE ?)' , "%#{session[:audit]['text']}%", "%#{session[:audit]['text']}%")
-    end
-
-    # User filter
-    if params[:user_id].present? and (params[:user_id].to_i > 0)
-      session[:audit]['user_id'] = params[:user_id]
-    end
-    if session[:audit]['user_id'].present?
-      @audit_logs = @audit_logs.where(user_id: session[:audit]['user_id'].to_i)
-    end
-
-    # Project filter
-    if params[:project_id].present? and (params[:project_id].to_i > 0)
-      session[:audit]['project_id'] = params[:project_id]
-    end
-    if session[:audit]['project_id'].present?
-      @audit_logs = @audit_logs.where(project_id: session[:audit]['project_id'].to_i)
-    end
-
-    # Certification path filter
-    if params[:certification_path_id].present? and (params[:certification_path_id].to_i > 0)
-      session[:audit]['certification_path_id'] = params[:certification_path_id]
-    end
-    if session[:audit]['certification_path_id'].present?
-      @audit_logs = @audit_logs.where(certification_path_id: session[:audit]['certification_path_id'].to_i)
-    end
-
-    # Date from filter
-    if params[:date_from].present?
-      session[:audit]['date_from'] = params[:date_from]
-      session[:audit]['tim_from'] = params[:time_from]
-    end
-    if session[:audit]['date_from'].present?
-      begin
-        @audit_logs = @audit_logs.where('created_at >= ?', DateTime.strptime(session[:audit]['date_from'] + ' ' + session[:audit]['tim_from'] + ':00', t('time.formats.filter')))
-      rescue ArgumentError
-        flash[:alert] = 'The date/time from fields contained invalid data.'
-      end
-    end
-
-    # Date to filter
-    if params[:date_to].present?
-      session[:audit]['date_to'] = params[:date_to]
-      session[:audit]['time_to'] = params[:time_to]
-    end
-    if session[:audit]['date_to'].present?
-      begin
-        @audit_logs = @audit_logs.where('created_at <= ?', DateTime.strptime(session[:audit]['date_to'] + ' ' + session[:audit]['time_to'] + ':59', t('time.formats.filter')))
-      rescue ArgumentError
-        flash[:alert] = 'The date/time to fields contained invalid data.'
-      end
-    end
-
-    # Only show user comments filter
-    if params[:only_user_comments].present?
-      session[:audit]['only_comments'] = true
+    if params[:reset].present?
+      session[:audit] = {'text' => nil, 'user_id' => nil, 'project_id' => nil, 'certification_path_id' => nil, 'date_from' => '', 'time_from' => '0:00', 'date_to' => '', 'time_to' => '0:00', 'only_comments' => false}
     else
-      session[:audit]['only_comments'] = false
-    end
-    if session[:audit]['only_comments'] == true
-      @audit_logs = @audit_logs.where.not(user_comment: nil)
+
+      if params[:button].present?
+        session[:audit] = {'text' => nil, 'user_id' => nil, 'project_id' => nil, 'certification_path_id' => nil, 'date_from' => '', 'time_from' => '0:00', 'date_to' => '', 'time_to' => '0:00', 'only_comments' => false}
+      else
+        session[:audit] ||= {'text' => nil, 'user_id' => nil, 'project_id' => nil, 'certification_path_id' => nil, 'date_from' => '', 'time_from' => '0:00', 'date_to' => '', 'time_to' => '0:00', 'only_comments' => false}
+      end
+
+      # Text filter
+      if params[:text].present?
+        session[:audit]['text'] = params[:text]
+      end
+      if session[:audit]['text'].present?
+        @audit_logs = @audit_logs.where('(system_message ILIKE ?) OR (user_comment ILIKE ?)' , "%#{session[:audit]['text']}%", "%#{session[:audit]['text']}%")
+      end
+
+      # User filter
+      if params[:user_id].present? and (params[:user_id].to_i > 0)
+        session[:audit]['user_id'] = params[:user_id]
+      end
+      if session[:audit]['user_id'].present?
+        @audit_logs = @audit_logs.where(user_id: session[:audit]['user_id'].to_i)
+      end
+
+      # Project filter
+      if params[:project_id].present? and (params[:project_id].to_i > 0)
+        session[:audit]['project_id'] = params[:project_id]
+      end
+      if session[:audit]['project_id'].present?
+        @audit_logs = @audit_logs.where(project_id: session[:audit]['project_id'].to_i)
+      end
+
+      # Certification path filter
+      if params[:certification_path_id].present? and (params[:certification_path_id].to_i > 0)
+        session[:audit]['certification_path_id'] = params[:certification_path_id]
+      end
+      if session[:audit]['certification_path_id'].present?
+        @audit_logs = @audit_logs.where(certification_path_id: session[:audit]['certification_path_id'].to_i)
+      end
+
+      # Date from filter
+      if params[:date_from].present?
+        session[:audit]['date_from'] = params[:date_from]
+        session[:audit]['tim_from'] = params[:time_from]
+      end
+      if session[:audit]['date_from'].present?
+        begin
+          @audit_logs = @audit_logs.where('created_at >= ?', DateTime.strptime(session[:audit]['date_from'] + ' ' + session[:audit]['tim_from'] + ':00', t('time.formats.filter')))
+        rescue ArgumentError
+          flash[:alert] = 'The date/time from fields contained invalid data.'
+        end
+      end
+
+      # Date to filter
+      if params[:date_to].present?
+        session[:audit]['date_to'] = params[:date_to]
+        session[:audit]['time_to'] = params[:time_to]
+      end
+      if session[:audit]['date_to'].present?
+        begin
+          @audit_logs = @audit_logs.where('created_at <= ?', DateTime.strptime(session[:audit]['date_to'] + ' ' + session[:audit]['time_to'] + ':59', t('time.formats.filter')))
+        rescue ArgumentError
+          flash[:alert] = 'The date/time to fields contained invalid data.'
+        end
+      end
+
+      # Only show user comments filter
+      if params[:only_user_comments].present?
+        session[:audit]['only_comments'] = true
+      else
+        session[:audit]['only_comments'] = false
+      end
+      if session[:audit]['only_comments'] == true
+        @audit_logs = @audit_logs.where.not(user_comment: nil)
+      end
     end
 
     @audit_logs = @audit_logs.paginate page: params[:page], per_page: 12
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,15 +9,19 @@ class TasksController < AuthenticatedController
       @projects = current_user.projects
     end
 
-    if params[:button].present?
+    if params[:reset].present?
       session[:task] = {'project_id' => nil}
     else
-      session[:task] ||= {'project_id' => nil}
-    end
+      if params[:button].present?
+        session[:task] = {'project_id' => nil}
+      else
+        session[:task] ||= {'project_id' => nil}
+      end
 
-    # Project filter
-    if params[:project_id].present? and (params[:project_id].to_i > 0)
-      session[:task]['project_id'] = params[:project_id]
+      # Project filter
+      if params[:project_id].present? and (params[:project_id].to_i > 0)
+        session[:task]['project_id'] = params[:project_id]
+      end
     end
 
     @tasks = TaskService::get_tasks(page: params[:page], per_page: 25, user: current_user, project_id: session[:task]['project_id'])
