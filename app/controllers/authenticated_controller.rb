7194bc4aab5c96fef3b9d54bdad5613023728106
Koen Dierckx <koen.dierckx@vito.be>
2015-11-02 13:14:12 +0000

    bugfix: make current user for auditable model
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@885 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -2,6 +2,7 @@ class AuthenticatedController < ApplicationController
   layout 'layouts/authenticated'
 
   before_action :authenticate_user!
+  before_action :set_current_user
 
   # https://github.com/CanCanCommunity/cancancan/wiki/Ensure-Authorization
   check_authorization :unless => :devise_controller?
@@ -9,5 +10,9 @@ class AuthenticatedController < ApplicationController
   rescue_from CanCan::AccessDenied do |exception|
     Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
     redirect_to forbidden_error_path
+  end
+
+  def set_current_user
+    User.current = current_user
   end
 end
