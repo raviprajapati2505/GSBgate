e8cb4951892b30702302deb8fd48e133a7bda0a1
Daniels Bart <bart.daniels@vito.be>
2015-11-04 07:40:54 +0000

    don't include audit logs in notification mail for GORD manager and GORD top manager
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@893 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -6,19 +6,26 @@ class DigestMailer < ApplicationMailer
   def digest_email(user)
     @user = user
 
-    if user.system_admin? || user.gord_manager? || user.gord_top_manager?
-      @audit_logs = AuditLog.where('updated_at > ?', Date.yesterday)
+    if user.gord_manager? || user.gord_top_manager?
+      # @audit_logs = AuditLog.where('updated_at > ?', Date.yesterday)
+      # NO AUDIT LOG
+      @audit_logs = []
+      @more_audit_logs = 0
     else
+      if user.system_admin?
+        @audit_logs = AuditLog.where('updated_at > ?', Date.yesterday)
+      else
       @audit_logs = AuditLog.where('updated_at > ?', Date.yesterday)
                             .where('project_id IN (select projects_users.project_id from projects_users where projects_users.user_id = ?)', user.id)
-    end
-    @more_audit_logs = @audit_logs.count - MAX_LOG_ITEMS
-    @more_audit_logs = @more_audit_logs < 0 ? 0 : @more_audit_logs
+      end
+      @more_audit_logs = @audit_logs.count - MAX_LOG_ITEMS
+      @more_audit_logs = @more_audit_logs < 0 ? 0 : @more_audit_logs
 
+      @audit_logs = @audit_logs.limit(MAX_LOG_ITEMS)
+    end
     @more_tasks = TaskService::count_tasks(user: user) - MAX_LOG_ITEMS
     @more_tasks = @more_tasks < 0 ? 0 : @more_tasks
 
-    @audit_logs = @audit_logs.limit(MAX_LOG_ITEMS)
     @tasks = TaskService::get_tasks(page: 1, per_page: MAX_LOG_ITEMS, user: user)
     mail(to: @user.email, subject: 'GSAS : progress report') unless (@tasks.empty? && @audit_logs.empty?)
   end
