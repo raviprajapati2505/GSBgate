4caf94ad0c44c1e7c87ef1d5d80a9ad74db16a12
Daniels Bart <bart.daniels@vito.be>
2015-11-04 13:00:42 +0000

    remember last notification datetime for each user
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@904 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -5,6 +5,7 @@ class DigestMailer < ApplicationMailer
 
   def digest_email(user)
     @user = user
+    user.last_notified_at ||= DateTime.new
 
     if user.gord_manager? || user.gord_top_manager?
       # @audit_logs = AuditLog.where('updated_at > ?', Date.yesterday)
@@ -13,9 +14,9 @@ class DigestMailer < ApplicationMailer
       @more_audit_logs = 0
     else
       if user.system_admin?
-        @audit_logs = AuditLog.where('updated_at > ?', Date.yesterday)
+        @audit_logs = AuditLog.where('updated_at > ?', user.last_notified_at)
       else
-      @audit_logs = AuditLog.where('updated_at > ?', Date.yesterday)
+      @audit_logs = AuditLog.where('updated_at > ?', user.last_notified_at)
                             .where('project_id IN (select projects_users.project_id from projects_users where projects_users.user_id = ?)', user.id)
       end
       @more_audit_logs = @audit_logs.count - MAX_LOG_ITEMS
@@ -28,6 +29,9 @@ class DigestMailer < ApplicationMailer
 
     @tasks = TaskService::get_tasks(page: 1, per_page: MAX_LOG_ITEMS, user: user)
     mail(to: @user.email, subject: 'GSAS : progress report') unless (@tasks.empty? && @audit_logs.empty?)
+
+    user.last_notified_at = DateTime.current
+    user.save!
   end
 
   def added_to_project_email(projectsuser)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151023083330) do
+ActiveRecord::Schema.define(version: 20151104112058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -326,6 +326,7 @@ ActiveRecord::Schema.define(version: 20151023083330) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "role"
+    t.datetime "last_notified_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
