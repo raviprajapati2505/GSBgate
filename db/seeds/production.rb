e397f8a20ff6306a093cb811b7b0ef85f459ccb2
Dhallé Kristof <kristof.dhalle@vito.be>
2015-11-16 13:00:13 +0000

    Added some production seed users. (bis)
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@935 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,4 +1,4 @@
-# VITO Admin
+# VITO users
 User.create!(email: 'sas@vito.be', password: 'password', password_confirmation: 'password', role: :system_admin)
 User.create!(email: 'karel.styns@vito.be', password: 'password', password_confirmation: 'password', role: :user)
 User.create!(email: 'bart.dooms@vito.be', password: 'password', password_confirmation: 'password', role: :user)
@@ -6,7 +6,7 @@ User.create!(email: 'koen.dierckx@vito.be', password: 'password', password_confi
 User.create!(email: 'bart.daniels@vito.be', password: 'password', password_confirmation: 'password', role: :user)
 User.create!(email: 'kristof.dhalle@vito.be', password: 'password', password_confirmation: 'password', role: :user)
 
-# GORD Users
+# GORD users
 User.create!(email: 'alhorr@gord.qa', password: 'password', password_confirmation: 'password', role: :gord_top_manager)
 User.create!(email: 'e.elsarrag@gord.qa', password: 'password', password_confirmation: 'password', role: :gord_manager)
 User.create!(email: 'o.khidir@gord.qa', password: 'password', password_confirmation: 'password', role: :system_admin)
@@ -20,3 +20,7 @@ User.create!(email: 'm.katafygiotou@gord.qa', password: 'password', password_con
 User.create!(email: 'r.elhayek@gord.qa', password: 'password', password_confirmation: 'password', role: :user)
 User.create!(email: 'm.chakravarthy@gord.qa', password: 'password', password_confirmation: 'password', role: :user)
 User.create!(email: 'r.dalistan@gord.qa', password: 'password', password_confirmation: 'password', role: :user)
+User.create!(email: 's.alayoubi@gord.qa', password: 'password', password_confirmation: 'password', role: :user)
+
+# DigitalNexa users
+User.create!(email: 'amit@digitalnexa.com', password: 'password', password_confirmation: 'password', role: :user)
