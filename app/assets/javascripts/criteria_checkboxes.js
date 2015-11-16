4d31feb3a6b7cc6435d215807d195321809d6679
Dhallé Kristof <kristof.dhalle@vito.be>
2015-11-16 14:51:50 +0000

    Fixed bug: duplicate event handlers after remote call.
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@938 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/assets/javascripts/criteria_checkboxes.js
+++ app/assets/javascripts/criteria_checkboxes.js
@@ -1,13 +1,13 @@
 $(function() {
     // Handle select criteria button click
-    $('.show-criteria-checkboxes').click(function(e) {
+    $('.show-criteria-checkboxes').off("click").click(function(e) {
         $(this).siblings('.criteria-checkboxes').show();
         $(this).hide();
         e.preventDefault();
     });
 
     // Handle scheme mix criteria lists toggle
-    $('ul.criteria-checkboxes li a').click(function (e) {
+    $('ul.criteria-checkboxes li a').off("click").click(function (e) {
         $(this).siblings('ul').slideToggle();
         $(this).children('i').toggleClass('fa-caret-square-o-right').toggleClass('fa-caret-square-o-down');
         e.preventDefault();
