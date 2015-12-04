b437d43ea414f0d60d76ce54ce26b37da0709dfc
Dhallé Kristof <kristof.dhalle@vito.be>
2015-12-04 10:08:59 +0000

    Bugfix.
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@983 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < AuthenticatedController
     @user = User.new(user_params)
 
     # Generate a random password, this will be changed by the user later
-    @user = Devise.friendly_token.first(20)
+    @user.password = Devise.friendly_token.first(20)
 
     if @user.save
       redirect_to users_path, notice: 'User account was successfully created. The user will be notified by email.'
