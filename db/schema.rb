58e7392fd73c9f4b9b6b040011900f0c209822e8
Koen Dierckx <koen.dierckx@vito.be>
2015-12-14 10:51:28 +0000

    renamed construction certificate to construction management certificate
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1034 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210122643) do
+ActiveRecord::Schema.define(version: 20151214104520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
