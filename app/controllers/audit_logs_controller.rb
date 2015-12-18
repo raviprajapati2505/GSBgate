5c41b6a1400cfc0a3bce5f2de036cf5e3313dc38
Dhallé Kristof <kristof.dhalle@vito.be>
2015-12-18 09:48:08 +0000

    Fixed bug where column in query was ambiguous.
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1044 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -57,7 +57,7 @@ class AuditLogsController < AuthenticatedController
       end
       if session[:audit]['date_from'].present?
         begin
-          @audit_logs = @audit_logs.where('created_at >= ?', DateTime.strptime(session[:audit]['date_from'] + ' ' + session[:audit]['tim_from'] + ':00', t('time.formats.filter')))
+          @audit_logs = @audit_logs.where('audit_logs.created_at >= ?', DateTime.strptime(session[:audit]['date_from'] + ' ' + session[:audit]['tim_from'] + ':00', t('time.formats.filter')))
         rescue ArgumentError
           flash[:alert] = 'The date/time from fields contained invalid data.'
         end
@@ -70,7 +70,7 @@ class AuditLogsController < AuthenticatedController
       end
       if session[:audit]['date_to'].present?
         begin
-          @audit_logs = @audit_logs.where('created_at <= ?', DateTime.strptime(session[:audit]['date_to'] + ' ' + session[:audit]['time_to'] + ':59', t('time.formats.filter')))
+          @audit_logs = @audit_logs.where('audit_logs.created_at <= ?', DateTime.strptime(session[:audit]['date_to'] + ' ' + session[:audit]['time_to'] + ':59', t('time.formats.filter')))
         rescue ArgumentError
           flash[:alert] = 'The date/time to fields contained invalid data.'
         end
