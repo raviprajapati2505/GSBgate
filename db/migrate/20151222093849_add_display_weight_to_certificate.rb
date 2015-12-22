358603c1e6fd07756bffbb7776df3af9abf5aa75
Koen Dierckx <koen.dierckx@vito.be>
2015-12-22 10:09:42 +0000

    added display weight to certificate
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1045 cf4ef618-bcd7-8443-a187-13cdf260f84e

---
+++ db/migrate/20151222093849_add_display_weight_to_certificate.rb
@@ -0,0 +1,23 @@
+class AddDisplayWeightToCertificate < ActiveRecord::Migration
+  def change
+    add_column :certificates, :display_weight, :integer
+  end
+
+  def up
+    loc_certificate = Certificate.find_by(certificate_type: Certificate.certificate_types[:design_type], assessment_stage: Certificate.assessment_stages[:design_stage])
+    loc_certificate.display_weight = 10
+    loc_certificate.save!
+
+    final_design_certificate = Certificate.find_by(certificate_type: Certificate.certificate_types[:design_type], assessment_stage: Certificate.assessment_stages[:construction_stage])
+    final_design_certificate.display_weight = 20
+    final_design_certificate.save!
+
+    construction_certificate = Certificate.find_by(certificate_type: Certificate.certificate_types[:construction_type], assessment_stage: Certificate.assessment_stages[:construction_stage])
+    construction_certificate.display_weight = 30
+    construction_certificate.save!
+
+    operations_certificate = Certificate.find_by(certificate_type: Certificate.certificate_types[:operations_type], assessment_stage: Certificate.assessment_stages[:operations_stage])
+    operations_certificate.display_weight = 40
+    operations_certificate.save!
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214104520) do
+ActiveRecord::Schema.define(version: 20151222093849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20151214104520) do
     t.integer  "assessment_stage"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "display_weight"
   end
 
   create_table "certification_path_statuses", force: :cascade do |t|
