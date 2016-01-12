55d7b989717cc89e823b670641c5a1b204f5a642
Dhallé Kristof <kristof.dhalle@vito.be>
2016-01-12 06:55:40 +0000

    Renamed "version" to "gsas_version".
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1077 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/models/effective/datatables/projects_certification_paths.rb
+++ app/models/effective/datatables/projects_certification_paths.rb
@@ -32,7 +32,7 @@ module Effective
         table_column 'owner_email', column: 'owner.email'
 
         #table_column 'certification_path_id', column: 'certification_paths.id', type: :integer, label: 'Certificate ID'
-        table_column 'certificate_id', column: 'certificates.id', label: 'Certificate', filter: {type: :select, values: Proc.new { Certificate.all.order(:display_weight).map { |certificate| [certificate.name + ' ' + certificate.version, certificate.id] } }} do |rec|
+        table_column 'certificate_id', column: 'certificates.id', label: 'Certificate', filter: {type: :select, values: Proc.new { Certificate.all.order(:display_weight).map { |certificate| [certificate.name + ' ' + certificate.gsas_version, certificate.id] } }} do |rec|
           if rec.certification_path_id.present?
             link_to(project_certification_path_path(rec.project_nr, rec.certification_path_id)) do
               rec.certificate_name
@@ -69,7 +69,7 @@ module Effective
         #   end
         # end
 
-        table_column 'schemes_array', label: 'Typologies', column: "ARRAY_TO_STRING(ARRAY(SELECT CONCAT(schemes.name, ' ', schemes.version) from schemes INNER JOIN scheme_mixes ON schemes.id = scheme_mixes.scheme_id WHERE scheme_mixes.certification_path_id = certification_paths.id), '|||')" do |rec|
+        table_column 'schemes_array', label: 'Typologies', column: "ARRAY_TO_STRING(ARRAY(SELECT CONCAT(schemes.name, ' ', schemes.gsas_version) from schemes INNER JOIN scheme_mixes ON schemes.id = scheme_mixes.scheme_id WHERE scheme_mixes.certification_path_id = certification_paths.id), '|||')" do |rec|
           rec.schemes_array.split('|||').sort.join('<br/>').html_safe unless rec.schemes_array.nil?
         end
         table_column 'assessors_array', label: 'Project Team', column: "ARRAY_TO_STRING(ARRAY(SELECT assessor_users.email FROM users as assessor_users INNER JOIN projects_users as assessor_project_users ON assessor_project_users.user_id = assessor_users.id  WHERE assessor_project_users.role IN (0,1) AND assessor_project_users.project_id = projects.id), '|||')" do |rec|
@@ -121,10 +121,10 @@ module Effective
                    .select('certification_paths.started_at as certification_path_started_at')
                    .select('certification_paths.certified_at as certification_path_certified_at')
                    .select('certification_paths.duration as certification_path_duration')
-                   .select("CONCAT(certificates.name, ' ', certificates.version) as certificate_name")
+                   .select("CONCAT(certificates.name, ' ', certificates.gsas_version) as certificate_name")
                    .select("certification_path_statuses.name as certification_path_status_name")
                    .select("CASE WHEN certification_path_statuses.id IS NULL THEN false WHEN certification_path_statuses.id = 15 THEN false WHEN certification_path_statuses.id = 16 THEN false ELSE true END as certification_path_status_is_active")
-                   .select("ARRAY_TO_STRING(ARRAY(SELECT CONCAT(schemes.name, ' ', schemes.version) from schemes INNER JOIN scheme_mixes ON schemes.id = scheme_mixes.scheme_id WHERE scheme_mixes.certification_path_id = certification_paths.id), '|||') AS schemes_array")
+                   .select("ARRAY_TO_STRING(ARRAY(SELECT CONCAT(schemes.name, ' ', schemes.gsas_version) from schemes INNER JOIN scheme_mixes ON schemes.id = scheme_mixes.scheme_id WHERE scheme_mixes.certification_path_id = certification_paths.id), '|||') AS schemes_array")
                    .select("ARRAY_TO_STRING(ARRAY(SELECT assessor_users.email FROM users as assessor_users INNER JOIN projects_users as assessor_project_users ON assessor_project_users.user_id = assessor_users.id  WHERE assessor_project_users.role IN (0,1) AND assessor_project_users.project_id = projects.id), '|||') AS assessors_array")
                    .select("ARRAY_TO_STRING(ARRAY(SELECT certifier_users.email FROM users as certifier_users INNER JOIN projects_users as certifier_project_users ON certifier_project_users.user_id = certifier_users.id  WHERE certifier_project_users.role IN (3,4) AND certifier_project_users.project_id = projects.id), '|||') AS certifiers_array")
                    .select("ARRAY_TO_STRING(ARRAY(SELECT enterprise_client_users.email FROM users as enterprise_client_users INNER JOIN projects_users as enterprise_client_project_users ON enterprise_client_project_users.user_id = enterprise_client_users.id  WHERE enterprise_client_project_users.role IN (2) AND enterprise_client_project_users.project_id = projects.id), '|||') AS enterprise_clients_array")
