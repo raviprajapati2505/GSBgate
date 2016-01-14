c4c59fe01c35cc294ca13a77a5299c05c3be226a
Daniels Bart <bart.daniels@vito.be>
2016-01-14 13:36:47 +0000

    added support ID (request ID) to error pages and logging statements to correlate them.
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1088 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.smtp_settings = { :address => 'smtp.vito.local' }
   config.action_mailer.asset_host = 'http://localhost:3000'
+
+  config.log_tags = [ :uuid ]
 end
