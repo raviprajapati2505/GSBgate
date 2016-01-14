808bc52ec77aab3e759f7db019e827658ef019c6
Koen Dierckx <koen.dierckx@vito.be>
2016-01-14 13:13:33 +0000

    Refactored scores, using score calculator concern, that does seperate database queries for improved performance Project and certificate table:   using improved score calculator   small bugfixes
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1087 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/models/concerns/score_calculator.rb
+++ app/models/concerns/score_calculator.rb
@@ -79,9 +79,9 @@ module ScoreCalculator
       relation = self.class.base_score_query
       case self.class.name
         when CertificationPath.name
-          relation.where(scheme_mixes: {certification_path_id: self.id})
+          relation.where(scheme_mixes_score: {certification_path_id: self.id})
         when SchemeMix.name
-          relation.where(scheme_mixes: {id: self.id})
+          relation.where(scheme_mixes_score: {id: self.id})
         when SchemeMixCriterion.name
           relation.where(id: self.id)
         else
@@ -98,15 +98,15 @@ module ScoreCalculator
 
     def group_template(group_type)
       if group_type == :scheme_mix_criteria_id
-        field_name = 'scheme_mix_criteria.id'
+        field_name = 'scheme_mix_criteria_score.id'
       elsif group_type == :certification_path_id
-        field_name = 'scheme_mixes.certification_path_id'
+        field_name = 'scheme_mixes_score.certification_path_id'
       elsif group_type == :scheme_mix_id
-        field_name = 'scheme_mix_criteria.scheme_mix_id'
+        field_name = 'scheme_mix_criteria_score.scheme_mix_id'
       elsif group_type == :scheme_criteria_id
-        field_name = 'scheme_mix_criteria.scheme_criterion_id'
+        field_name = 'scheme_mix_criteria_score.scheme_criterion_id'
       elsif group_type == :scheme_category_id
-        field_name = 'scheme_criteria.scheme_category_id'
+        field_name = 'scheme_criteria_score.scheme_category_id'
       else
         raise('Unexpected group type: ' + group_type.to_s)
       end
@@ -116,20 +116,20 @@ module ScoreCalculator
     def query_score_template(point_type, field_name, field_alias = '')
       # determine template
       if point_type == :criteria_points
-        template = 'SUM(GREATEST(%{field_table}.%{field_name}::float, scheme_criteria.minimum_score::float)) %{field_alias}'
+        template = 'SUM(GREATEST(%{field_table}.%{field_name}::float, scheme_criteria_score.minimum_score::float)) %{field_alias}'
       elsif point_type == :scheme_points
-        template = 'SUM(((GREATEST(%{field_table}.%{field_name}::float, scheme_criteria.minimum_score::float) / scheme_criteria.maximum_score::float) * ((3.0 * (scheme_criteria.weight + scheme_criteria.incentive_weight)) / 100.0))) %{field_alias}'
+        template = 'SUM(((GREATEST(%{field_table}.%{field_name}::float, scheme_criteria_score.minimum_score::float) / scheme_criteria_score.maximum_score::float) * ((3.0 * (scheme_criteria_score.weight + scheme_criteria_score.incentive_weight)) / 100.0))) %{field_alias}'
       elsif point_type == :certificate_points
-        template = 'SUM(((GREATEST(%{field_table}.%{field_name}::float, scheme_criteria.minimum_score::float) / scheme_criteria.maximum_score::float) * ((3.0 * (scheme_criteria.weight + scheme_criteria.incentive_weight)) / 100.0)  * (scheme_mixes.weight / 100.0))) %{field_alias}'
+        template = 'SUM(((GREATEST(%{field_table}.%{field_name}::float, scheme_criteria_score.minimum_score::float) / scheme_criteria_score.maximum_score::float) * ((3.0 * (scheme_criteria_score.weight + scheme_criteria_score.incentive_weight)) / 100.0)  * (scheme_mixes_score.weight / 100.0))) %{field_alias}'
       else
         raise('Unexpected point type: ' + point_type.to_s)
       end
 
       # determine table for field name
       if field_name == :maximum_score or field_name == :minimum_score
-        field_table = 'scheme_criteria'
+        field_table = 'scheme_criteria_score'
       elsif field_name == :achieved_score or field_name == :submitted_score or field_name == :targeted_score
-        field_table = 'scheme_mix_criteria'
+        field_table = 'scheme_mix_criteria_score'
       else
         raise('Unexpected score field: ' + field_name.to_s)
       end
@@ -142,11 +142,17 @@ module ScoreCalculator
     end
 
     def base_score_query
+      relation = SchemeMixCriterion
+                     .from('scheme_mix_criteria as scheme_mix_criteria_score')
+                     .joins('INNER JOIN scheme_criteria as scheme_criteria_score ON scheme_criteria_score.id = scheme_mix_criteria_score.scheme_criterion_id')
+                     .joins('INNER JOIN scheme_mixes as scheme_mixes_score ON scheme_mixes_score.id = scheme_mix_criteria_score.scheme_mix_id')
+                     .joins('INNER JOIN certification_paths as certification_paths_score ON certification_paths_score.id = scheme_mixes_score.certification_path_id')
       case self.name
         when CertificationPath.name, SchemeMix.name, SchemeMixCriterion.name
-          SchemeMixCriterion.joins(:scheme_criterion).joins(:scheme_mix)
+          relation
         when Effective::Datatables::ProjectsCertificationPaths.name
-          SchemeMixCriterion.joins(:scheme_criterion).joins(:scheme_mix).where('scheme_mixes.certification_path_id = certification_paths.id')
+          relation
+              .where('scheme_mixes_score.certification_path_id = certification_paths.id')
         else
           raise('Unsupported class: ', self.name)
       end
