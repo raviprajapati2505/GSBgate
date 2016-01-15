6476db34cddcea397567584ea30f5cba83dd3845
Koen Dierckx <koen.dierckx@vito.be>
2016-01-15 08:01:15 +0000

    bugfix
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1091 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -246,15 +246,18 @@ module ApplicationHelper
   end
 
   def sum_score_hashes(score_hashes)
-    score_hashes.inject(Hash.new()) { |total, score|
-      total.merge(score) { |k, (a, b)|
-        # if any value is nil, the end result shall be nil
-        if (a.nil? || b.nil?)
-          res = nil
+    # create a result hash, using the same keys as the input hashes, but with zero values
+    total_score = score_hashes[0].dup
+    total_score.each{|k,v| total_score[k]=0}
+    # Sum the input hashes
+    score_hashes.inject(total_score) { |total, score|
+      total.each{|k,v|
+        # if any input has a nil for a given key, the sum shall be nil also !!
+        if (total[k].nil? || score[k].nil?)
+          total[k] = nil
         else
-          res = a + b
+          total[k] = total[k] + score[k]
         end
-        res
       }
     }
   end
