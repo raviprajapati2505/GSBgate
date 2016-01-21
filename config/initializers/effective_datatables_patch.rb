0859035782bb278006652781a55094234ffad9dd
Koen Dierckx <koen.dierckx@vito.be>
2016-01-21 09:06:30 +0000

    bugfix, ajax calls didn't use cancan ability
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1117 cf4ef618-bcd7-8443-a187-13cdf260f84e

---
+++ config/initializers/effective_datatables_patch.rb
@@ -0,0 +1,40 @@
+module Effective
+  class DatatablesController < ApplicationController
+    def show
+      @datatable = find_datatable(params[:id]).try(:new, params[:attributes])
+      @datatable.current_ability = current_ability
+      @datatable.view = view_context if !@datatable.nil?
+
+      EffectiveDatatables.authorized?(self, :index, @datatable.try(:collection_class) || @datatable.try(:class))
+
+      respond_to do |format|
+        format.html
+        format.json {
+          if Rails.env.production?
+            render :json => (@datatable.to_json rescue error_json)
+          else
+            render :json => @datatable.to_json
+          end
+        }
+      end
+    end
+
+    private
+
+    def find_datatable(id)
+      id_plural = id.pluralize == id && id.singularize != id
+      klass = "effective/datatables/#{id}".classify
+
+      (id_plural ? klass.pluralize : klass).safe_constantize
+    end
+
+    def error_json
+      {
+          :draw => params[:draw].to_i,
+          :data => [],
+          :recordsTotal => 0,
+          :recordsFiltered => 0,
+      }.to_json
+    end
+  end
+end
