524dcf0581f79712f248a58afc3f256abf4c3896
Koen Dierckx <koen.dierckx@vito.be>
2016-03-21 11:54:51 +0000

    updated gmaps version removed parameter (as it is no longer required)
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1216 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,7 +21,7 @@ module ApplicationHelper
 
   def use_gmaps_for(filename)
     content_for(:js) do
-      javascript_include_tag 'https://maps.google.com/maps/api/js?v=3.13&sensor=false&libraries=geometry',
+      javascript_include_tag 'https://maps.google.com/maps/api/js?v=3&libraries=geometry',
                              'https://google-maps-utility-library-v3.googlecode.com/svn/tags/markerclustererplus/2.0.14/src/markerclusterer_packed.js',
                              'maps/_gmaps',
                              "maps/#{filename}"
