54562f8feefa0f2f6a1ce2a8544adc3c5c31ca9a
Dhallé Kristof <kristof.dhalle@vito.be>
2016-03-30 14:15:12 +0000

    Notify the user if his CGP license has expired. (bis)
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1274 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -11,9 +11,9 @@ User.create!(username: 'dr.youssef@example.com', password: 'password', email: 's
 User.create!(username: 'omeima@example.com', password: 'password', email: 'sas@vito.be', linkme_user: false, role: :gsas_trust_admin, name: 'GSAS trust admin')
 
 # GSAS trust
-User.create!(username: 'bart.daniels@vito.be', password: 'password', email: 'bart.daniels@vito.be', linkme_user: false, role: :default_role, cgp_license: true, gord_employee: true, name: 'Bart Daniels')
-User.create!(username: 'karel.styns@vito.be', password: 'password', email: 'karel.styns@vito.be', linkme_user: false, role: :default_role, cgp_license: false, gord_employee: true, name: 'Karel Styns')
+User.create!(username: 'bart.daniels@vito.be', password: 'password', email: 'bart.daniels@vito.be', linkme_user: false, role: :default_role, cgp_license: true, cgp_license_expired: false, gord_employee: true, name: 'Bart Daniels')
+User.create!(username: 'karel.styns@vito.be', password: 'password', email: 'karel.styns@vito.be', linkme_user: false, role: :default_role, cgp_license: false, cgp_license_expired: false, gord_employee: false, name: 'Karel Styns')
 
 # Project team
-User.create!(username: 'koen.dierckx@vito.be', password: 'password', email: 'koen.dierckx@vito.be', linkme_user: false, role: :default_role, cgp_license: true, gord_employee: false, name: 'Koen Dierckx')
-User.create!(username: 'kristof.dhalle@vito.be', password: 'password', email: 'kristof.dhalle@vito.be', linkme_user: false, role: :default_role, cgp_license: false, gord_employee: false, name: 'Kristof Dhallé')
+User.create!(username: 'koen.dierckx@vito.be', password: 'password', email: 'koen.dierckx@vito.be', linkme_user: false, role: :default_role, cgp_license: true, cgp_license_expired: false, gord_employee: false, name: 'Koen Dierckx')
+User.create!(username: 'kristof.dhalle@vito.be', password: 'password', email: 'kristof.dhalle@vito.be', linkme_user: false, role: :default_role, cgp_license: false, cgp_license_expired: true, gord_employee: false, name: 'Kristof Dhallé')
