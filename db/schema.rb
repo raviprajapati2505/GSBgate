5e7c5599283f1f6d73c39521bfe4f048b5d760ce
Daniels Bart <bart.daniels@vito.be>
2016-04-06 09:58:03 +0000

    bug fix : last_notified_at user attribute was missing for the digest mailer
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1312 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160406063435) do
+ActiveRecord::Schema.define(version: 20160406092028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -381,6 +381,7 @@ ActiveRecord::Schema.define(version: 20160406063435) do
     t.text     "picture"
     t.string   "name"
     t.boolean  "cgp_license_expired", default: false, null: false
+    t.datetime "last_notified_at"
   end
 
   add_index "users", ["linkme_member_id"], name: "index_users_on_linkme_member_id", unique: true, using: :btree
