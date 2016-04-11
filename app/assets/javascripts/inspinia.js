00fca4dabcbd7e842ebc1347d400f38d9f09fcaf
Koen Dierckx <koen.dierckx@vito.be>
2016-04-11 10:20:27 +0000

    renamed, so it won't show up at TODO list
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1325 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/assets/javascripts/inspinia.js
+++ app/assets/javascripts/inspinia.js
@@ -87,7 +87,7 @@ $(document).ready(function () {
         railOpacity: 0.4
     });
 
-    // Small todo handler
+    // Small to-do handler
     $('.check-link').click(function () {
         var button = $(this).find('i');
         var label = $(this).next('span');
