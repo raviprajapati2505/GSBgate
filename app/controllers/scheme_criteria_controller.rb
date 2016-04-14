3882a686614662814f258d4f8534e9fa497ddce1
Koen Dierckx <koen.dierckx@vito.be>
2016-04-14 09:21:01 +0000

    allow editting criterion names
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1351 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/controllers/scheme_criteria_controller.rb
+++ app/controllers/scheme_criteria_controller.rb
@@ -15,4 +15,19 @@ class SchemeCriteriaController < AuthenticatedController
   def show
     @page_title = ERB::Util.html_escape(@scheme_criterion.full_name)
   end
+
+  def update
+    if @scheme_criterion.update(scheme_criterion_params)
+      redirect_to scheme_criterion_path(@scheme_criterion), notice: 'Criterion was successfully updated.'
+    else
+      render action: :edit, alert: 'Criterion could not be updated.'
+    end
+  end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def scheme_criterion_params
+    params.require(:scheme_criterion).permit(:name)
+  end
 end
