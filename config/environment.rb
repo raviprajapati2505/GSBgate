89a9387d9847595fbb8f3b1698ee5b0f21609128
Koen Dierckx <koen.dierckx@vito.be>
2016-04-22 07:50:27 +0000

    fixed inflections
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1365 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActiveSupport::Inflector.inflections do |inflect|
-  inflect.irregular 'criterion', 'criteria'
-  inflect.irregular 'datum', 'data'
-end
-
 APP_VERSION = IO.popen('svnversion').readlines[0] rescue ''
