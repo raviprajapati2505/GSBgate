07b46d972dc20868713fff6b04c443760e7731a1
Dhallé Kristof <kristof.dhalle@vito.be>
2016-09-08 09:44:58 +0000

    Removed unused JS.
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1408 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,7 +22,6 @@ module ApplicationHelper
   def use_gmaps_for(filename)
     content_for(:js) do
       javascript_include_tag 'https://maps.google.com/maps/api/js?v=3&libraries=geometry',
-                             'https://google-maps-utility-library-v3.googlecode.com/svn/tags/markerclustererplus/2.0.14/src/markerclusterer_packed.js',
                              'maps/_gmaps',
                              "maps/#{filename}"
     end
