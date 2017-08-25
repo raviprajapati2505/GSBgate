d660bae0f175f30871ebf77c55bf968b44f5240e
Daniels Bart <bart.daniels@vito.be>
2017-08-25 09:08:20 +0000

    Bug fix : E.6 criteria for Healthcare should be MO.10
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1486 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- db/migrate/20170824130102_move_vertical_transportation_crit.rb
+++
@@ -1,6 +0,0 @@
-class MoveVerticalTransportationCrit < ActiveRecord::Migration
-  def change
-    scheme_category = SchemeCategory.select(:id).joins(:scheme).where(code: 'MO', schemes: {gsas_version: '2.0', name: 'Healthcare'}).first
-    SchemeCriterion.joins(scheme_category: [:scheme]).where(name: 'Vertical Transportation', scheme_categories: {code: 'E'}, schemes: {gsas_version: '2.0'}).update_all(number: 10, scheme_category_id: scheme_category.id)
-  end
-end
