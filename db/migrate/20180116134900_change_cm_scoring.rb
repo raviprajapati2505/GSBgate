6c7057e7d55827084b32b6b6225dee6bb530eac2
Daniels Bart <bart.daniels@vito.be>
2018-01-24 15:26:07 +0000

    bug fix migration scripts
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1591 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- db/migrate/20180116134900_change_cm_scoring.rb
+++ db/migrate/20180116134900_change_cm_scoring.rb
@@ -7,27 +7,6 @@ class ChangeCmScoring < ActiveRecord::Migration
     # migrate existing scheme_mix_criterion data
     SchemeMixCriterion.joins(scheme_criterion: {scheme_category: {scheme: {development_types: :certificate}}}).where.not(scheme_criteria: {name: 'Dust Control', number: 1}, 'scheme_categories.code' => 'OE').where.not("certificates.certificate_type = 1 AND certificates.gsas_version = 'v2.1 Issue 3.0' AND certificates.certification_type IN (30, 31, 32, 33)").update_all(incentive_scored: true)
 
-    # OE.1
-    scheme_criteria = SchemeCriterion.joins(scheme_category: [scheme: [development_types: [:certificate]]]).where(name: 'Dust Control', number: 1, scheme_categories: {code: 'OE'}).where("certificates.certificate_type = 1 AND certificates.gsas_version = 'v2.1 Issue 3.0' AND certificates.certification_type IN (31, 32, 33)").distinct
-    scheme_criteria.each do |scheme_criterion|
-      scheme_criterion.incentive_weight_0 = 2.0
-      scheme_criterion.incentive_weight_1 = 2.0
-      scheme_criterion.incentive_weight_2 = 2.0
-      scheme_criterion.incentive_weight_3 = 2.0
-      scheme_criterion.calculate_incentive = false
-      scheme_criterion.assign_incentive_manually = true
-      scheme_criterion.save!
-    end
-
-    # MO.4
-    scheme_criteria = SchemeCriterion.joins(scheme_category: [scheme: [development_types: [:certificate]]]).where(name: 'Workers Accommodation', number: 4, scheme_categories: {code: 'MO'}).where("certificates.certificate_type = 1 AND certificates.gsas_version = 'v2.1 Issue 3.0' AND certificates.certification_type IN (31, 32, 33)").distinct
-    scheme_criteria.each do |scheme_criterion|
-      scheme_criterion.incentive_weight_1 = 2.0
-      scheme_criterion.incentive_weight_2 = 3.0
-      scheme_criterion.incentive_weight_3 = 4.0
-      scheme_criterion.save!
-    end
-
     remove_column :scheme_criteria, :incentive_weight
   end
 end
