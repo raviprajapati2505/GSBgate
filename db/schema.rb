ba1e9af79c452bf7e358a22fc156c3ebb8436d83
Dhallé Kristof <kristof.dhalle@vito.be>
2018-02-01 15:00:05 +0000

    GORD-30: Disable all previous changes as requested by GORD. Team members should see all criteria even if they are not assigned to it.
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1615 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180129070155) do
+ActiveRecord::Schema.define(version: 20180201124522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,7 +113,7 @@ ActiveRecord::Schema.define(version: 20180129070155) do
     t.integer  "max_review_count",             default: 2
     t.integer  "development_type_id"
     t.string   "signed_certificate_file"
-    t.boolean  "show_all_criteria",            default: false
+    t.boolean  "show_all_criteria",            default: true
     t.datetime "expires_at"
   end
 
