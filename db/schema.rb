e9bb8cfbffe934a45069ae53ec8f730d12fdbfc9
Daniels Bart <bart.daniels@vito.be>
2018-08-06 11:32:51 +0000

    Add criterion, requirements and criterion texts
    git-svn-id: svn://svnext.vito.be/ENE/TEM/EMIS/GORD-GSAS/trunk/src@1642 cf4ef618-bcd7-8443-a187-13cdf260f84e

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180719115043) do
+ActiveRecord::Schema.define(version: 20180806095411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
