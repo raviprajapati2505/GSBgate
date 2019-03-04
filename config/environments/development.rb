1ad5fd4bcd733bcdfc0fc921003246dae8453071
Leite Pinto De Carvalho Augusto <augusto.leitepintodecarvalho@vito.be>
2019-03-04 13:29:25 +0100

    javascript compile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,17 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
