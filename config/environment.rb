538a8bb5fd8459891772688eb6c8b95f300b483b
Dhallé Kristof <kristof.dhalle@vito.be>
2019-03-13 15:16:35 +0100

    Read current Git commit hash from Capistrano's REVISION file for app version.

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# Use current Git commit hash as app version
-APP_VERSION = IO.popen('git rev-parse HEAD').readlines[0] rescue ''
+# Read current Git commit hash from Capistrano's REVISION file
+APP_VERSION = IO.popen('cat REVISION').readlines[0] rescue ''
