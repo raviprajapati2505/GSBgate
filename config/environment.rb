f47f9496381197946b16a6bcabf7ce41e30f6359
Dhallé Kristof <kristof.dhalle@vito.be>
2019-03-13 15:06:31 +0100

    Use current Git commit hash as app version.

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-APP_VERSION = IO.popen('svnversion').readlines[0] rescue ''
+# Use current Git commit hash as app version
+APP_VERSION = IO.popen('git rev-parse HEAD').readlines[0] rescue ''
