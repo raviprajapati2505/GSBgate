cea8781b3d8cf621bbcb51f0f5657dc920d21f30
Dhallé Kristof <kristof.dhalle@vito.be>
2019-04-05 13:17:17 +0200

    Updated effective_datatables gem to highest version that's compatible with Bootstrap 3.

--- config/initializers/effective_datatables.rb
+++ config/initializers/effective_datatables.rb
@@ -2,54 +2,44 @@ EffectiveDatatables.setup do |config|
   # Authorization Method
   #
   # This method is called by all controller actions with the appropriate action and resource
-  # If the method returns false, an Effective::AccessDenied Error will be raised (see README.md for complete info)
+  # If it raises an exception or returns false, an Effective::AccessDenied Error will be raised
   #
-  # Use via Proc (and with CanCan):
-  # config.authorization_method = Proc.new { |controller, action, resource| authorize!(action, resource) }
+  # Use via Proc:
+  # Proc.new { |controller, action, resource| authorize!(action, resource) }       # CanCan
+  # Proc.new { |controller, action, resource| can?(action, resource) }             # CanCan with skip_authorization_check
+  # Proc.new { |controller, action, resource| authorize "#{action}?", resource }   # Pundit
+  # Proc.new { |controller, action, resource| current_user.is?(:admin) }           # Custom logic
   #
-  # Use via custom method:
+  # Use via Boolean:
+  # config.authorization_method = true  # Always authorized
+  # config.authorization_method = false # Always unauthorized
+  #
+  # Use via Method (probably in your application_controller.rb):
   # config.authorization_method = :my_authorization_method
-  #
-  # And then in your application_controller.rb:
-  #
-  # def my_authorization_method(action, resource)
-  #   current_user.is?(:admin)
+  # def my_authorization_method(resource, action)
+  #   true
   # end
-  #
-  # Or disable the check completely:
-  # config.authorization_method = false
-
-  # config.authorization_method = Proc.new { |controller, action, resource| true } # All users can see every screen
-  config.authorization_method = Proc.new { |controller, action, resource| authorize!(action, resource) } # Use cancancan authorization
-
-  # Date & DateTime Format
-  # By default, format Date and DateTime values with the following
-  config.date_format = '%Y-%m-%d'
-  config.datetime_format = '%Y-%m-%d %H:%M'
-
-  # Boolean format
-  # If :yes_no, display as yes_no instead of true/false
-  config.boolean_format = :yes_no
-
-  # Format integer columns with the following proc or function
-  config.integer_format = :number_with_delimiter
+  config.authorization_method = Proc.new { |controller, action, resource| authorize!(action, resource) }
 
   # Default number of entries shown per page
-  # Valid options are: 10, 25, 50, 100, 250, 1000, :all
-  config.default_entries = 25
+  # Valid options are: 5, 10, 25, 50, 100, 250, 500, :all
+  config.default_length = 25
+
+  # Default class used on the <table> tag
+  config.html_class = 'table table-bordered table-striped table-hover'
+
+  # If a user has previously visited this page and is returning, use the cookie to restore last session
+  # Irregardless of this setting, effective_datatables still uses a cookie to function
+  config.save_state = true
 
   # When using the actions_column DSL method, apply the following behavior
   # Valid values for each action are:
-  # true (always include a link to this action)
-  # false (never include a link to this action)
-  # :authorize (include link only if authorized - only works with ActiveRecord collections)
+  # true - display this action if authorized?(:show, Post)
+  # false - do not display this action
+  # :authorize - display this action if authorized?(:show, Post<3>)  (every instance is checked)
   #
-  # When :authorize, a single check will be made via the above authorization_method
-  # to see if the current_user has permission to :show/:edit/:destroy the collection class, i.e. Post
-  # EffectiveDatatables.authorized? (:show || :edit || :destroy), Post
-  #
-  # For performance reasons, we only check the class once, not every individual resource.
-  # You can override this default by calling `actions_column show: false, edit: true, destroy: :authorize`
+  # You can override these defaults on a per-table basis
+  # by calling `actions_column(show: false, edit: true, destroy: :authorize)`
   config.actions_column = {
     show: :authorize,
     edit: :authorize,
@@ -57,4 +47,9 @@ EffectiveDatatables.setup do |config|
     unarchive: :authorize
   }
 
+  # Log search/sort information to the console
+  config.debug = true
+
+  # String size. Final byte size is about 1.5 times bigger, after rails signs it
+  config.max_cookie_size = 2000
 end
