8e8148e23cad4be6f7f054eac48968b834505b2b
Bart Daniels <bart.daniels@vito.be>
2019-04-05 14:31:34 +0200

    testing x_sendfile with staging config

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -2,6 +2,10 @@ class ArchivesController < AuthenticatedController
   load_and_authorize_resource
 
   def show
-    send_file(@archive.archive_path, type: 'application/zip', disposition: 'attachment', filename: @archive.archive_file, x_sendfile: true)
+    # send_file(@archive.archive_path, type: 'application/zip', disposition: 'attachment', filename: @archive.archive_file, x_sendfile: true)
+    response.set_header('X-Sendfile', @archive.archive_path)
+    response.set_header('Content-Type', 'application/zip')
+    response.set_header('Content-Disposition', "attachment; filename=\"#{@archive.archive_file}\"")
+    return response
   end
 end
