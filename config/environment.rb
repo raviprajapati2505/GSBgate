e9aad35b1314ef14c9acdd4b899003751dfac8e0
Bart Daniels <bart.daniels@vito.be>
2019-04-05 13:50:12 +0200

    testing x_sendfile with staging config

--- config/environment.rb
+++ config/environment.rb
@@ -7,4 +7,4 @@ Rails.application.initialize!
 # Read current Git commit hash from Capistrano's REVISION file
 APP_VERSION = IO.popen('cat REVISION').readlines[0] rescue ''
 
-# ActionController::Streaming::X_SENDFILE_HEADER = 'X-Sendfile'
+ActionController::Streaming::X_SENDFILE_HEADER = 'X-Sendfile'
