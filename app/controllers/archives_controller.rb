405f80a8a0e4b5799fcf6c7af10bb1dfc0dcd979
Bart Daniels <bart.daniels@vito.be>
2019-04-05 11:28:56 +0200

    testing x_sendfile with other config option

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -2,6 +2,6 @@ class ArchivesController < AuthenticatedController
   load_and_authorize_resource
 
   def show
-    send_file(@archive.archive_path, type: 'application/zip', disposition: 'attachment', filename: @archive.archive_file, x_sendfile: false)
+    send_file(@archive.archive_path, type: 'application/zip', disposition: 'attachment', filename: @archive.archive_file, x_sendfile: true)
   end
 end
