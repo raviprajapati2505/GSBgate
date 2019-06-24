8dcd93be782ca23ad1b73efa68e429780323d8e4
Dhallé Kristof <kristof.dhalle@vito.be>
2019-06-24 08:43:49 +0200

    Added CORS resource for authentication.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,14 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
              expose: ['Authorization']
   end
   allow do
+    origins 'localhost:4200', 'gord.vito.be', 'www.gsas.qa'
+
+    resource '/users/*',
+             methods: [:get, :post, :options, :delete],
+             headers: :any,
+             expose: ['Authorization']
+  end
+  allow do
     origins 'localhost:3000', 'gord.vito.be', 'www.gsas.qa'
 
     resource '*',
