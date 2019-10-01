b593c9aafe0ff737e8ff66e7212acac18ecffd35
Bart Daniels <bart.daniels@vito.be>
2019-10-01 11:14:50 +0200

    bugfix : bug in checking if appeal is permitted

--- app/controllers/certification_paths_controller.rb
+++ app/controllers/certification_paths_controller.rb
@@ -167,7 +167,7 @@ class CertificationPathsController < AuthenticatedController
       if todos.blank?
         # Check if there's an appeal
         # For construction management 2019 appeal can only be requested during stage 3
-        unless certification_path_params.has_key?(:appealed) && @certification_path.certificate.construction_2019? && !@certification_path.construction_certificate_stage3?
+        unless certification_path_params.has_key?(:appealed) && @certification_path.certificate.construction_2019? && !@certification_path.certificate.construction_certificate_stage3?
           @certification_path.appealed = certification_path_params.has_key?(:appealed)
         end
 
