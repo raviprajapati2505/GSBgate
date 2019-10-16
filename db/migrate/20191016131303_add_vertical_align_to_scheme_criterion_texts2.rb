0a807f717e1c8aab3c7bcf17ae6ac3fd7d337526
Bart Daniels <bart.daniels@vito.be>
2019-10-16 15:46:53 +0200

    GORD-179 : vertical alignment in formula's

---
+++ db/migrate/20191016131303_add_vertical_align_to_scheme_criterion_texts2.rb
@@ -0,0 +1,7 @@
+class AddVerticalAlignToSchemeCriterionTexts2 < ActiveRecord::Migration[5.2]
+  def change
+    SchemeCriterionText.where("html_text LIKE '%<td rowspan=\"2\">%'").each do |sct|
+      sct.update_column(:html_text, sct.html_text.gsub('<td rowspan="2">', '<td rowspan="2" style="vertical-align:middle;">'))
+    end
+  end
+end
