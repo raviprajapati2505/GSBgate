ce056375f153967bf17bb2e27d69c86d81303357
Bart Daniels <bart.daniels@vito.be>
2019-10-17 15:35:05 +0200

    GORD-188 : subscripts missing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_120648) do
+ActiveRecord::Schema.define(version: 2019_10_17_124211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
