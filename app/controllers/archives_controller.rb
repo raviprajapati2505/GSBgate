c311ab8e73d7e6a69c8d8a0a35a53e18420a0d10
vishalgord <v.dixit@gord.qa>
2020-07-23 17:21:01 +0530

    archive download fix

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -2,11 +2,11 @@ class ArchivesController < AuthenticatedController
   load_and_authorize_resource
 
   def show
-    # send_file(@archive.archive_path, type: 'application/zip', disposition: 'attachment', filename: @archive.archive_file, x_sendfile: true)
-    response.headers['X-Sendfile'] = @archive.archive_path
-    response.headers['Content-Length'] = '0'
-    response.headers['Content-Type'] = 'application/zip'
-    response.headers['Content-Disposition'] = "attachment; filename=\"#{@archive.archive_file}\""
-    render body: nil, status: :ok
+    send_file(@archive.archive_path, type: 'application/zip', disposition: 'attachment', filename: @archive.archive_file, x_sendfile: true)
+    # response.headers['X-Sendfile'] = @archive.archive_path
+    # response.headers['Content-Length'] = '0'
+    # response.headers['Content-Type'] = 'application/zip'
+    # response.headers['Content-Disposition'] = "attachment; filename=\"#{@archive.archive_file}\""
+    # render body: nil, status: :ok
   end
 end
