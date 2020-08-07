07f056e40bfac86af8dff018b504452ab8f70521
Pratik Panchal <pratik.panchal@bacancytechnology.com>
2020-08-07 13:07:57 +0530

    changes for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
              expose: ['Authorization']
   end
   allow do
-    origins 'localhost:3000', 'gord.vito.be', 'www.gsas.qa', 'gctprojects.qa'
+    origins 'localhost:3000', 'gord.vito.be', 'www.gsas.qa', 'https://gctprojects.qa', "http://gctprojects.qa"
 
     resource '*',
              methods: :any,
