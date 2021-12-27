54a8e5a0223660f50b5cd14938d11db46f436c04
bacancy-bhargavthummar <bhargav.thummar@bacancy.com>
2021-12-27 10:36:09 +0530

    Changed header and footer of criteria summary pdf

--- app/models/reports/criteria_scores.rb
+++ app/models/reports/criteria_scores.rb
@@ -18,7 +18,6 @@ class Reports::CriteriaScores < Reports::BaseReport
   TEXT_COLOR = 'ffffff'.freeze
   MAIN_COLOR = '62A744'.freeze
   BACKGROUND_COLOR = 'EEEEEE'.freeze
-  HEADER_LOGO = 'gsas_logo.jpg'.freeze
   FOOTER_LOGO = 'gord_logo_black.jpg'.freeze
   STAR_ICON = 'green_star.png'.freeze
   FOOTER_URL = "<link href='http://www.gord.qa'>www.gord.qa</link>".freeze
@@ -28,8 +27,9 @@ class Reports::CriteriaScores < Reports::BaseReport
   def initialize(scheme_mix)
     # A4:: => 595.28 x 841.89 pt
     @document = Prawn::Document.new(page_size: 'A4',
-                                    page_layout: :portrait,
-                                    margin: [170, PAGE_MARGIN, 110, PAGE_MARGIN])
+    page_layout: :portrait,
+    margin: 40)
+
     @scheme_mix = scheme_mix
     @certification_path = @scheme_mix.certification_path
     @score = @scheme_mix.scores_in_scheme_points[:achieved_score_in_scheme_points]
@@ -43,13 +43,15 @@ class Reports::CriteriaScores < Reports::BaseReport
   def do_render
     font 'Times-Roman', size: 11
 
+    bounding_box([@document.bounds.left, @document.bounds.top - 80], width: @document.bounds.width) do
+      newline
+      draw_certificate_header
+    end
     draw_headers
 
     bounding_box([0, 560], width: @document.bounds.width) do
       # Draw awarded target
       # draw_awarded_target
-      newline
-
 
       # Draw all category/criteria tables
       rows_on_page = 0
@@ -63,97 +65,53 @@ class Reports::CriteriaScores < Reports::BaseReport
           rows_on_page += row_count
         end
 
-        newline
         draw_criteria_table(category_with_criteria[:category], category_with_criteria[:criteria])
         newline
       end
     end
-
+    newline(2)
     draw_footers
   end
 
-  # def draw_headers
-  #   canvas do
-  #     repeat(:all) do
-  #       # Certificate Information
-  #       bounding_box([PAGE_MARGIN, 800], width: 400) do
-  #         text "#{@certification_path.name} Criteria Summary", size: 20, color: MAIN_COLOR
-  #         text @scheme_mix.scheme.full_name, size: 20, color: MAIN_COLOR
-  #         text "#{@certification_path.project.name}", size: 16, color: MAIN_COLOR
-  #       end
-
-  #       # Logo
-  #       logo_width = 80
-  #       bounding_box([@document.bounds.width - logo_width - PAGE_MARGIN, 800], width: logo_width) do
-  #         image image_path(HEADER_LOGO), height: logo_width
-  #       end
-
-  #       # Project information
-  #       bounding_box([PAGE_MARGIN, 720], width: @document.bounds.width - (PAGE_MARGIN * 2)) do
-  #         newline
-  #         # table(
-  #         #     [[
-  #         #          "<color rgb='#{MAIN_COLOR}'>Date:</color> #{@certification_path.certified_at.strftime('%B %d, %Y') if @certification_path.certified_at.present?}",
-  #         #          "<color rgb='#{MAIN_COLOR}'>Project ID:</color> #{@certification_path.project.code}"
-  #         #      ]],
-  #         #     width: @document.bounds.width,
-  #         #     cell_style: {
-  #         #         background_color: 'EAEAEA',
-  #         #         border_color: 'BABABB',
-  #         #         size: 10,
-  #         #         font_style: :bold,
-  #         #         inline_format: true
-  #         #     }
-  #         # )
-  #         draw_project_info
-  #       end
-  #     end
-  #   end
-  # end
-
   def draw_headers
-    repeat(:all) do
-      bounding_box([@document.bounds.left, @document.bounds.top], width: 120, height: HEADER_HEIGHT) do
-        image image_path(HEADER_LOGO), width: 120
-      end
-      bounding_box([@document.bounds.right - 50, @document.bounds.top], width: 60, height: HEADER_HEIGHT) do
-        image image_path(GSAS_LOGO), width: 60
-      end
-      newline
-      bounding_box([@document.bounds.right - 90, @document.bounds.top - 60], width: 100, height: HEADER_HEIGHT) do
-        text = "Issued Date: #{DateTime.current.to_date}\n"
-        text2 = "Ref: LOC/QA 2532-2343-RU"
+    canvas do
+      repeat(:all) do
+        # Certificate Information
+        bounding_box([@document.bounds.left + 50, @document.bounds.top - 50], width: 120, height: HEADER_HEIGHT) do
+          image image_path(HEADER_LOGO), width: 100
+        end
 
-        styled_text("<div style='font-size: 8; text-align: right'>#{text}<br />#{text2}</div>")
-      end
-      bounding_box([@document.bounds.right - 380, @document.bounds.bottom + 20], width: 120, height: HEADER_HEIGHT) do
-        text = "Qatar Science & Technology Park | Tech 1 | Level 2 \n"
-        text2 = "Suite 203 | P.O. Box: 210162| Doha - Qatar \n"
-        text3 = "T: +974 4404 9010 F: +974 4404 9002"
+        # Logo
+        logo_width = 50
+        bounding_box([@document.bounds.width - logo_width - PAGE_MARGIN, 800], width: logo_width) do
+          image image_path(GSAS_LOGO), height: logo_width
+        end
 
-        styled_text("<div style='font-size: 7; text-align: right'>#{text}#{text2}#{text3}</div>")
-       
-        stroke do
-          # vertical_line 50, 100, at: [125, 125]
-          vertical_line 30, 80, at: 125
-          move_down 50
+        # Issued date and ref no.
+        newline
+        bounding_box([@document.bounds.right - 150, @document.bounds.top - 100], width: 100, height: HEADER_HEIGHT) do
+          text = "Issued Date: #{DateTime.current.to_date}\n"
+          text2 = "Ref: LOC/QA 2532-2343-RU"
+
+          styled_text("<div style='font-size: 8; text-align: right'>#{text}<br />#{text2}</div>")
+        end
+
+        # Project information
+        bounding_box([PAGE_MARGIN, 680], width: @document.bounds.width - (PAGE_MARGIN * 2)) do
+          newline
+          draw_project_info
         end
       end
-      bounding_box([@document.bounds.right - 250, @document.bounds.bottom + 20], width: 120, height: HEADER_HEIGHT) do
-        # text = "واحة قطر للعلوم والتكنولوجيا | تك 1 | المستوي 2 \n"
-        # text2 = "جناح 203 | ص. صندوق: 210162 | الدوحة قطر \n"
-        # text3 = "هاتف: +974 4404 9010 فاكس: +974 4404 9002"
-
-        text = "Qatar Science & Technology Park | Tech 1 | Level 2 \n"
-        text2 = "Suite 203 | P.O. Box: 210162| Doha - Qatar \n"
-        text3 = "T: +974 4404 9010 F: +974 4404 9002"
-
-        styled_text("<div style='font-size: 7; text-align: left'>#{text}#{text2}#{text3}</div>")
-      end
-      bounding_box([@document.bounds.right - 50, @document.bounds.bottom + 100], width: 50, height: HEADER_HEIGHT) do
-        image image_path(GSAS_LOGO), width: 50
-      end
     end
+  end
+
+  def draw_certificate_header
+    text = "#{@certification_path.name} Criteria Summary"
+    text2 = @scheme_mix.scheme.full_name
+    text3 = "#{@certification_path.project.name}"
+
+    styled_text("<div style='font-size: 12; text-align: center; color: #{MAIN_COLOR}'>#{text}<br />#{text2}<br />#{text3}</div>")
+    newline
   end
 
   def draw_footers
@@ -164,25 +122,48 @@ class Reports::CriteriaScores < Reports::BaseReport
         # self.line_width = 6
         # stroke_horizontal_line @document.bounds.left + PAGE_MARGIN, @document.bounds.right - PAGE_MARGIN, at: 100
 
+        # Slogan
+        bounding_box([@document.bounds.left + 50, 50], width: 100, height: 10) do
+          text 'Crafting a Green Legacy', inline_format: true, size: 9
+        end
+
+        # Address
+        bounding_box([@document.bounds.right - 400, @document.bounds.bottom + 70], width: 120, height: HEADER_HEIGHT) do
+          text = "Qatar Science & Technology Park | Tech 1 | Level 2 \n"
+          text2 = "Suite 203 | P.O. Box: 210162| Doha - Qatar \n"
+          text3 = "T: +974 4404 9010 F: +974 4404 9002"
+  
+          styled_text("<div style='font-size: 7; text-align: right'>#{text}<br />#{text2}<br />#{text3}</div>")
+         
+          stroke do
+            # vertical_line 50, 100, at: [125, 125]
+            vertical_line 30, 80, at: 125
+            move_down 50
+          end
+        end
+        bounding_box([@document.bounds.right - 270, @document.bounds.bottom + 70], width: 120, height: HEADER_HEIGHT) do
+  
+          text = "Qatar Science & Technology Park | Tech 1 | Level 2 \n"
+          text2 = "Suite 203 | P.O. Box: 210162| Doha - Qatar \n"
+          text3 = "T: +974 4404 9010 F: +974 4404 9002"
+  
+          styled_text("<div style='font-size: 7; text-align: left'>#{text}<br />#{text2}<br />#{text3}</div>")
+        end
+
+        # GSAS LOGO
+        bounding_box([@document.bounds.right - 110, @document.bounds.bottom + 150], width: 50, height: 100) do
+          image image_path(GSAS_LOGO), width: 50
+        end
+
         # URL
-        bounding_box([PAGE_MARGIN, 70], width: 50, height: 10) do
+        bounding_box([@document.bounds.right - 110, @document.bounds.bottom + 50], width: 70, height: HEADER_HEIGHT) do
           text FOOTER_URL, inline_format: true, size: 9
         end
 
-        # Logo
-        logo_width = 70
-        bounding_box([@document.bounds.right - logo_width - PAGE_MARGIN, 80], width: logo_width, height: logo_width) do
-          image image_path(FOOTER_LOGO), width: logo_width
-        end
 
         # Thin line
         self.line_width = 1
         stroke_horizontal_line bounds.left, bounds.right, at: 30
-      end
-
-      # Page number
-      bounding_box([270, 70], width: 50) do
-        number_pages '<page> / <total>', align: :center, size: 9
       end
     end
   end
@@ -212,11 +193,11 @@ class Reports::CriteriaScores < Reports::BaseReport
   def draw_criteria_table(scheme_category, scheme_mix_criteria)
     # Prepare table data
     data = []
-    data.append([{content: "#{scheme_category.name.upcase} [#{scheme_category.code}]", rowspan: scheme_mix_criteria.size + 1}, 'Criterion', 'Level', 'Remarks'])
+    data.append([{content: "#{scheme_category.name.upcase} [#{scheme_category.code}]", rowspan: scheme_mix_criteria.size + 1}, 'Criterion', 'Level', 'Achieved', 'Remarks'])
 
     # Add the category rows to the table
     scheme_mix_criteria.each do |smc|
-      data.append([smc.full_name, number_with_precision(smc.achieved_score, precision: 0, significant: true), t(smc.status, scope: 'activerecord.attributes.scheme_mix_criterion.statuses')])
+      data.append([smc.full_name, number_with_precision(smc.achieved_score, precision: 0, significant: true), number_with_precision(smc.achieved_score, precision: 0, significant: true), t(smc.status, scope: 'activerecord.attributes.scheme_mix_criterion.statuses')])
     end
 
     # Output table
@@ -263,12 +244,12 @@ class Reports::CriteriaScores < Reports::BaseReport
         cells.border_top_color = '000000'
         cells.border_bottom_color = '000000'
 
-        # Set column widths
-        column(0).width = width * 0.21
-        column(1).width = width * 0.37
-        column(2).width = width * 0.16
-        column(3).width = width * 0.26
-        # column(4).width = width * 0.25
+        column(0).width = width * 0.20
+        column(1).width = width * 0.35
+        column(2).width = width * 0.10
+        column(3).width = width * 0.10
+        column(4).width = width * 0.25
+  
 
         # Misc table border style
         row(0).border_top_color = '000000'
