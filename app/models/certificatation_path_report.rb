b01aefb619b4c92d1744daf29dd1a8c6b0cec950
bacancy-bhargavthummar <bhargav.thummar@bacancy.com>
2022-02-03 14:55:29 +0530

    Create seed file to generate certification_report_information for certified certificates.

--- app/models/certificatation_path_report.rb
+++ app/models/certificatation_path_report.rb
@@ -1,5 +1,5 @@
 class CertificatationPathReport < ApplicationRecord
   belongs_to :certification_path
 
-  validates :to, :reference_number, :project_owner, :designation, :service_provider, :project_name, :project_location, :issuance_date, :approval_date, presence: true
+  validates :to, :reference_number, :project_owner, :service_provider, :project_name, :project_location, :issuance_date, :approval_date, presence: true
 end
