7eb3839aefeed0671785a66767f7305d02ba7d10
bacancy-bhargavthummar <bhargav.thummar@bacancy.com>
2022-04-04 19:32:44 +0530

    Add select service provider field in users forms.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -78,12 +78,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email username employer_name password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email username employer_name service_provider_id password password_confirmation])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email username linkme_user gord_employee cgp_license cgp_license_expired employer_name active password password_confirmation current_password])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name email username linkme_user gord_employee cgp_license cgp_license_expired employer_name service_provider_id active password password_confirmation current_password])
   end
 
   def sp_sign_up_params
