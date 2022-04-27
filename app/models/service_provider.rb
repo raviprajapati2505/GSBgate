59b3583ac12cb528f10e7ee07d8a9e94ebdc70f9
bacancy-bhargavthummar <bhargav.thummar@bacancy.com>
2022-04-27 16:27:58 +0530

    Add basic conditions for cgp & cep according certificate_types.

--- app/models/service_provider.rb
+++ app/models/service_provider.rb
@@ -5,11 +5,39 @@ class ServiceProvider < User
     access_licences.joins(:licence).where("DATE(access_licences.expiry_date) > :current_date AND licences.licence_type = 'ServiceProviderLicence'", current_date: Date.today)
   end
 
+  def valid_service_provider_design_build_licences
+    access_licences.joins(:licence).where("DATE(access_licences.expiry_date) > :current_date AND licences.licence_type = 'ServiceProviderLicence' AND licences.certificate_type = :certificate_type", current_date: Date.today, certificate_type: Certificate.certificate_types[:design_type])
+  end
+
+  def valid_service_provider_construction_management_licences
+    access_licences.joins(:licence).where("DATE(access_licences.expiry_date) > :current_date AND licences.licence_type = 'ServiceProviderLicence' AND licences.certificate_type = :certificate_type", current_date: Date.today, certificate_type: Certificate.certificate_types[:construction_type])
+  end
+
+  def valid_service_provider_operation_licences
+    access_licences.joins(:licence).where("DATE(access_licences.expiry_date) > :current_date AND licences.licence_type = 'ServiceProviderLicence' AND licences.certificate_type = :certificate_type", current_date: Date.today, certificate_type: Certificate.certificate_types[:operations_type])
+  end
+
   def valid_cgps
     users.joins(access_licences: :licence).where("DATE(access_licences.expiry_date) > :current_date AND licences.licence_type = 'CgpLicence'", current_date: Date.today)
   end
 
   def valid_ceps
     users.joins(access_licences: :licence).where("DATE(access_licences.expiry_date) > :current_date AND licences.licence_type = 'CepLicence'", current_date: Date.today)
+  end
+
+  def valid_design_build_cgps
+    users.joins(access_licences: :licence).where("DATE(access_licences.expiry_date) > :current_date AND licences.licence_type = 'CgpLicence' AND licences.certificate_type = :certificate_type", current_date: Date.today, certificate_type: Certificate.certificate_types[:design_type])
+  end
+
+  def valid_design_build_ceps
+    users.joins(access_licences: :licence).where("DATE(access_licences.expiry_date) > :current_date AND licences.licence_type = 'CepLicence' AND licences.certificate_type = :certificate_type", current_date: Date.today, certificate_type: Certificate.certificate_types[:design_type])
+  end
+
+  def valid_construction_management_cgps
+    users.joins(access_licences: :licence).where("DATE(access_licences.expiry_date) > :current_date AND licences.licence_type = 'CgpLicence' AND licences.certificate_type = :certificate_type", current_date: Date.today, certificate_type: Certificate.certificate_types[:construction_type])
+  end
+
+  def valid_construction_management_ceps
+    users.joins(access_licences: :licence).where("DATE(access_licences.expiry_date) > :current_date AND licences.licence_type = 'CepLicence' AND licences.certificate_type = :certificate_type", current_date: Date.today, certificate_type: Certificate.certificate_types[:construction_type])
   end
 end
