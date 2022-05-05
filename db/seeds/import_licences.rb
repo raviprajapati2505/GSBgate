1c6892658ede2505e1f476099841fe2106b7a3f5
Bhargav Thummar <bhargav.thummar@bacancy.com>
2022-05-05 17:10:54 +0530

    Optimization of import script and minor bug fixes

--- db/seeds/import_licences.rb
+++ db/seeds/import_licences.rb
@@ -1,4 +1,4 @@
-require 'roo'
+equire 'roo'
 
 xlsx = Roo::Excelx.new("#{Rails.root}/db/imports/licence_allocation_sample.xlsx")
 header = xlsx.row(2)
@@ -9,67 +9,96 @@ user_licences_sheet = xlsx.sheet_for('CGPs')
 xlsx.each_with_pagename do |name, sheet|
 
     if(name == 'ServiceProviders')
-        (4..xlsx.last_row).each do |i|
-            row = Hash[[header, xlsx.row(4)].transpose]
-            user_service_provider = User.find_or_initialize_by(email: row["email"])
-            user_service_provider.name ||= row["name"].to_s
-            user_service_provider.username ||= row["username"].to_s
-            user_service_provider.password = 'test#1234'
-            user_service_provider.active = true
-            user_service_provider.role = 'service_provider'
-            user_service_provider.type = 'ServiceProvider'
-            user_service_provider.skip_confirmation!
-            user_service_provider.save
-        end
-    end
+        sp_errors = []
 
+        (4..xlsx.last_row).each do |i|
+            row = Hash[[header, xlsx.row(i)].transpose]
+            email = row["email"]&.squish&.strip&.downcase
+            service_provider = User.find_or_initialize_by(email: email)
+            service_provider.name ||= row["name"].to_s
+            service_provider.username ||= row["username"].to_s
+            service_provider.password = 'test#1234'
+            service_provider.active = true
+            service_provider.role = 'service_provider'
+            service_provider.type = 'ServiceProvider'
+            service_provider.skip_confirmation!
+
+            unless service_provider.save
+                sp_errors << "Row: #{i}, Email: #{service_provider.email}, Message: #{service_provider.errors.full_messages}"
+            end
+        end
+
+        puts "--------------------------Service Provider Errors----------------------------"
+        puts "Error Count #{sp_errors.size}"
+        puts sp_errors.join("\n")
+    end
     if(name == 'Service Provider-CGPs')
+        cp_errors = []
         header_for_cgp = user_cgp_sheet.row(2)
+
         (4..user_cgp_sheet.last_row).each do |i|
             row_new = Hash[[header_for_cgp, user_cgp_sheet.row(i)].transpose]
-            service_provider_exists = ServiceProvider.find_by(email: row_new["service_provider_email"])
-            if !service_provider_exists.nil?
-                
-                new_users = row_new["user_email"].split(',')
+            email = row_new["service_provider_email"]&.squish&.strip&.downcase
+            service_provider = ServiceProvider.find_by(email: email)
+
+            if service_provider.present?
+                new_users = row_new["user_email"].split(',').map(&:squish).map(&:strip)
+
                 new_users.each do |email|
                     user = User.find_or_initialize_by(email: email.downcase)
                     user.name ||= email
                     user.username ||= email
                     user.password = 'test#1234'
-                    user.service_provider_id = service_provider_exists.id
+                    user.service_provider_id = service_provider.id
                     user.active = true
-                    #@user.role = 'service_provider'
                     user.skip_confirmation!
-                    user.save
+
+                    unless user.save
+                        # enable this if the case case of username already exists in database 
+                        # user = User.find_by(username: email.downcase)
+                        # user.service_provider_id = service_provider.id
+                        # user.save
+                        cp_errors << "Row: #{i}, Email: #{user.email}, Message: #{user.errors.full_messages}"
+                    end
                 end
             end
         end
+
+        puts "\n\n\n"
+        puts "--------------------------CP users Errors----------------------------"
+        puts "Error Count #{cp_errors.size}"
+        puts cp_errors.join("\n")
     end
 
     if(name == 'CGPs')
+        cp_licences_errors = []
         header_for_licences = user_licences_sheet.row(5)
-        (7..user_licences_sheet.last_row).each do |index_licence|
-            row_licence = Hash[[header_for_licences, user_licences_sheet.row(index_licence)].transpose]
-            user_exists = User.find_by(email: row_licence["Email"].downcase)
-            if !user_exists.nil?
+        (7..user_licences_sheet.last_row).each do |i|
+            row_licence = Hash[[header_for_licences, user_licences_sheet.row(i)].transpose]
+            user = User.find_by(email: row_licence["Email"]&.squish&.strip&.downcase)
+
+            if user.present?
                 type_of_licences.each do |type|
-                    if !row_licence[type].nil? && row_licence[type] != '-'
-                        #puts type
-                        d = row_licence[type].to_s
-                        fulldate = d.to_date
-                        licence_data = Licence.find_by(title: type)
-                        if !licence_data.nil?
-                            access_licences = AccessLicence.find_or_initialize_by(licence: licence_data, user: user_exists)
-                            #puts access_licences.id
-                            access_licences.user = user_exists
-                            access_licences.licence = licence_data
-                            access_licences.expiry_date = fulldate
-                            access_licences.save
-                        end
+                    if row_licence[type].present? && row_licence[type].to_s.squish&.strip != '-'
+                        formatted_date = row_licence[type].to_s
+                        expiry_date = formatted_date.to_date
+                        licence = Licence.find_by(title: type)
+
+                        access_licence = user.access_licences.find_or_initialize_by(licence_id: licence&.id)
+                        access_licence.expiry_date = expiry_date
                     end
                 end
-            end    
+
+                unless user.save
+                    cp_licences_errors << "Row: #{i}, Email: #{user.email}, Message: #{user.errors.full_messages}"
+                end
+            end
         end
+
+        puts "\n\n\n"
+        puts "--------------------------Licences Errors----------------------------"
+        puts "Error Count #{cp_licences_errors.size}"
+        puts cp_licences_errors.join("\n")
     end
 
     def type_of_licences
