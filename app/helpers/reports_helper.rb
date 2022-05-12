bdf1a3b192960164849594af014aa040555cc102
Bhargav Thummar <bhargav.thummar@bacancy.com>
2022-05-12 13:19:56 +0530

    code optimization for certification intro and summary text

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -34,4 +34,48 @@ module ReportsHelper
 
     return {project_type: project_type, certificate_name: certificate_name}
   end
+
+  def certificate_intro_text(certificate_name, certificate_stage = '')
+    case certificate_name
+    when 'Letter of Conformance'
+      text = "This is to notify that GSAS Trust has reviewed the project based on the submitted information. The project is found eligible to receive the Design Certificate as a provision for final GSAS Design & Build Certificate in the form of \"Letter of Conformance (LOC)\", The project is achieving: \n"
+    when 'GSAS-CM', 'Construction Certificate'
+      case certificate_stage
+        when 'Stage 1: Foundation'
+          text = "This is to notify that GSAS Trust has reviewed the construction submittals in accordance with the latest GSAS Construction Management assessments and has completed the Initial Site Audit requirements of Construction Stage 1 (Foundation). The project is found eligible to receive the First Interim Audit Advisory Notice (AAN) No.01 achieving the following: \n"
+        when 'Stage 2: Substructure & Superstructure'
+          text = "This is to notify that GSAS Trust has reviewed the construction submittals in accordance with the latest GSAS Construction Management assessments and has completed the Second Site Audit requirements of Construction Stage 2 (Substructure & Superstructure). The project is found eligible to receive the Second Interim Audit Advisory Notice (AAN) No.02 achieving the following: \n"
+        when 'Stage 3: Finishing'
+          text = "This is to notify that GSAS Trust has reviewed the construction submittals in accordance with the latest GSAS Construction Management assessments and has completed the Third Site Audit requirements of Construction Stage 3 (Finishing). The project is found eligible to receive the Third Interim Audit Advisory Notice (AAN) No.03 achieving the following: \n"
+      end
+    else
+    end
+  end
+
+  def certificate_summary_text(certificate_name, certificate_stage = '')
+    case certificate_name 
+      when 'Letter of Conformance'
+        text = { "1" => "The summary of the obtained rating is attached herewith.", 
+          "2" => "This letter is only the predecessor towards achieving the final GSAS-D&B Certificate and should not be considered as the final certificate. The project should satisfy during the construction stage all the requirements of <b>Conformance to Design Audit(CDA)</b> which is a pre-requisite for the final GSAS-D&B Certificate as indicated in GSAS Technical Guide, <span style='color: #337ab7'>www.gsas.gord.qa</span> \n", 
+          "3" => "In the event of any future changes applied to the criteria pertaining to this issued certificate, the changes are required to be re-assessed once again.", 
+          "4" => "Finally, congratulations on partaking in this noble endeavor. We look forward to creating jointly a healthy and sustainable future." 
+        }
+      when 'GSAS-CM', 'Construction Certificate' 
+        case certificate_stage
+          when 'Stage 1: Foundation'
+            interim_count = 'First'
+          when 'Stage 2: Substructure & Superstructure'
+            interim_count = 'Second'
+          when 'Stage 3: Finishing'
+            interim_count = 'Third'
+          else
+            interim_count = 'First'
+        end
+        text = { "1" => "Criteria summary of the #{interim_count} Interim Audit Advisory Notice is attached herewith.", 
+          "2" => "This notice is only the predecessor towards achieving the final GSAS-CM Certificate and should not be considered as the final certificate. The project shall satisfy during the rest of the construction stages all the requirements which is a pre-requisite for the GSAS-CM Certificate as stipulated in GSAS Technical Guide, <span style='color: #337ab7'>www.gsas.gord.qa</span> \n", 
+          "3" => "Finally, congratulations on partaking in this noble endeavor. We look forward to creating jointly a healthy and sustainable future."
+        }
+      else
+      end
+  end
 end
