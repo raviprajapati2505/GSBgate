7426e87174f37995b015ec23300d79fa3c59443a
bacancy-bhargavthummar <bhargav.thummar@bacancy.com>
2022-07-14 17:12:17 +0530

    Add multi-select filters in offline projects.

--- app/models/effective/datatables/offline_projects.rb
+++ app/models/effective/datatables/offline_projects.rb
@@ -2,6 +2,7 @@ module Effective
   module Datatables
     class OfflineProjects < Effective::Datatable
       include ActionView::Helpers::TranslationHelper
+
       datatable do
         col :code, label: t('models.effective.datatables.projects.lables.project_code') do |rec| 
           link_to_if(!current_user.record_checker?,
@@ -17,8 +18,17 @@ module Effective
           )
         end
 
-        col :certificate_type, col_class: 'multiple-select', label: t('models.effective.datatables.offline.project.certificate_type'), search: { as: :select, collection: Proc.new { Offline::Project.certificate_types.map { |k, v| [k, v] } } } do |rec|
+        col :certificate_type, col_class: 'multiple-select', sql_column: 'offline_projects.certificate_type', label: t('models.effective.datatables.offline.project.certificate_type'), search: { as: :select, collection: Proc.new { Offline::Project.certificate_types.map { |k, v| [k, v] } } } do |rec|
           rec.certificate_type
+
+        end.search do |collection, terms, column, index|
+          terms_array = terms.split(",")
+
+          unless (collection.class == Array || terms_array.include?(""))
+            collection.where("offline_projects.certificate_type IN (?)", terms_array)
+          else
+            collection
+          end
         end
 
         col :certified_area, label: t('models.effective.datatables.offline.project.certified_area')
@@ -29,12 +39,14 @@ module Effective
 
         col :construction_year, label: t('models.effective.datatables.offline.project.construction_year')
 
-        col :certification_name, col_class: 'multiple-select', sql_column: 'offline_certification_paths.name', label: t('models.effective.datatables.offline.certification_path.name'), search: { as: :select, collection: Proc.new { get_certificate_types_names(current_user) } } do |rec| link_to_if(!current_user.record_checker?,
+        col :certification_name, col_class: 'multiple-select', sql_column: 'offline_certification_paths.name', label: t('models.effective.datatables.offline.certification_path.name'), search: { as: :select, collection: Certificate.all.order(:display_weight).map { |certificate| [certificate.stage_title, certificate.stage_title.sub(',', '+')] }.uniq } do |rec| 
+          link_to_if(!current_user.record_checker?,
             rec.certification_name,
             offline_project_certification_path(rec.id, rec.certification_id)
           )
         end.search do |collection, terms, column, index|
-          terms_array = terms.split(",")
+          terms_array = terms.split(',').map { |ele| ele.gsub('+', ',') || ele }
+
           unless (collection.class == Array || terms_array.include?(""))
             collection.where("offline_certification_paths.name IN (?)", terms_array)
           else
@@ -42,12 +54,15 @@ module Effective
           end
         end
 
-        col :certification_version, col_class: 'multiple-select', sql_column: 'offline_certification_paths.version', label: t('models.effective.datatables.offline.certification_path.version'), search: { as: :select, collection: Proc.new { Offline::CertificationPath.versions.map { |k, v| [k, k] } } } do |rec|
+        col :certification_version, col_class: 'multiple-select', sql_column: 'offline_certification_paths.version', label: t('models.effective.datatables.offline.certification_path.version'), search: { as: :select, collection: Proc.new { Offline::CertificationPath.versions.map { |k, v| [k, v] } } } do |rec|
           link_to_if(!current_user.record_checker?,
             Offline::CertificationPath.versions.keys[rec.certification_version],
-            offline_project_certification_path(rec.id, rec.certification_id))
+            offline_project_certification_path(rec.id, rec.certification_id)
+          )
+
         end.search do |collection, terms, column, index|
           terms_array = terms.split(",")
+
           unless (collection.class == Array || terms_array.include?(""))
             collection.where("offline_certification_paths.version IN (?)", terms_array)
           else
@@ -55,11 +70,43 @@ module Effective
           end
         end
 
-        col :certification_development_type, col_class: 'multiple-select', sql_column: 'offline_certification_paths.development_type', label: t('models.effective.datatables.offline.certification_path.development_type'), search: { as: :select, collection: Proc.new { DevelopmentType.select(:name, :display_weight).order(:display_weight).distinct.map { |development_type| [development_type.name, development_type.name] }.uniq } }
+        col :certification_development_type, col_class: 'multiple-select', sql_column: 'offline_certification_paths.development_type', label: t('models.effective.datatables.offline.certification_path.development_type'), search: { as: :select, collection: Proc.new { DevelopmentType.select(:name, :display_weight).order(:display_weight).distinct.map { |development_type| [development_type.name, development_type.name] }.uniq } } do |rec|
+          rec.certification_development_type
 
-        col :certification_rating, partial: '/offline/certification_paths/rating', sql_column: 'offline_certification_paths.rating', partial_as: 'rec', col_class: 'multiple-select', label: t('models.effective.datatables.offline.certification_path.rating'), search: { as: :select, collection: Proc.new { Offline::CertificationPath.ratings.map { |k, v| [k.titleize, k] }.uniq } } 
+        end.search do |collection, terms, column, index|
+          terms_array = terms.split(",")
+          unless (collection.class == Array || terms_array.include?(""))
+            collection.where("offline_certification_paths.development_type IN (?)", terms_array)
+          else
+            collection
+          end
+        end
 
-        col :certification_status, col_class: 'multiple-select', sql_column: 'offline_certification_paths.status', label: t('models.effective.datatables.offline.certification_path.status'), search: { as: :select, collection: Proc.new { CertificationPathStatus.all.map { |status| [status.name, status.name] }.uniq } }
+        col :certification_rating, col_class: 'multiple-select', sql_column: 'offline_certification_paths.rating', label: t('models.effective.datatables.offline.certification_path.rating'), search: { as: :select, collection: Proc.new { Offline::CertificationPath.ratings.map { |k, v| [k.titleize, k] } } } do |rec|
+          render partial: "/offline/certification_paths/rating.html.erb", locals: { ratings: rec.certification_rating }
+
+        end.search do |collection, terms, column, index|
+          terms_array = terms.split(",")
+
+          unless (collection.class == Array || terms_array.include?(""))
+            collection.where("offline_certification_paths.rating IN (?)", terms_array.map(&:to_i))
+          else
+            collection
+          end
+        end
+
+        col :certification_status, col_class: 'multiple-select', sql_column: 'offline_certification_paths.status', label: t('models.effective.datatables.offline.certification_path.status'), search: { as: :select, collection: Proc.new { CertificationPathStatus.all.map { |status| [status.name, status.name] }.uniq } } do |rec|
+          rec.certification_status
+
+        end.search do |collection, terms, column, index|
+          terms_array = terms.split(",")
+
+          unless (collection.class == Array || terms_array.include?(""))
+            collection.where("offline_certification_paths.status IN (?)", terms_array)
+          else
+            collection
+          end
+        end
 
         col :certification_certified_at, label: t('models.effective.datatables.offline.certification_path.certified_at')
 
