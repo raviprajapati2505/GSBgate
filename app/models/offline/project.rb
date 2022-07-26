da8c4e1eb6d65d45a758b37f39470810bd05e707
Ravi Prajapati <ravi.prajapati@bacancy.com>
2022-07-26 16:44:49 +0530

    Bug fixes: Offline project seed file remove validation

--- app/models/offline/project.rb
+++ app/models/offline/project.rb
@@ -3,7 +3,7 @@ module Offline
     has_many :offline_project_documents, class_name: 'Offline::ProjectDocument', foreign_key: 'offline_project_id', inverse_of: :offline_project, dependent: :destroy
     has_many :offline_certification_paths, class_name: 'Offline::CertificationPath', foreign_key: 'offline_project_id', inverse_of: :offline_project, dependent: :destroy
 
-    validates :code, :name, :developer, :certificate_type, :certified_area, :plot_area, presence: true
+    validates :code, :name, :developer, :certificate_type, presence: true
 
     enum certificate_type:
       [
