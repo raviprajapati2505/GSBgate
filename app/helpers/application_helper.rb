d90af0c69015ca22e6b995538c1c6acf363e34d5
Ravi Prajapati <ravi.prajapati@bacancy.com>
2022-11-15 21:19:20 +0530

    Prawn gem PDF issue fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -937,4 +937,24 @@ module ApplicationHelper
         '<span class="certi-sub-status-badge status-badge-op"'+ image_tag('/icons/certi-sub-status-op.png') +'</span>'+rec.certification_status
     end
   end
+
+  FONTS_DIR = '/app/assets/fonts/reports'
+  IMAGES_DIR = '/app/assets/images/reports/'
+
+  def newline(amount = 1)
+    text "\n" * amount
+  end
+
+  def font_path(filename)
+    "#{Rails.root}#{FONTS_DIR}/#{filename}"
+  end
+
+  def image_path(filename)
+    "#{Rails.root}#{IMAGES_DIR}/#{filename}"
+  end
+
+  def save_as(file_name)
+    FileUtils.mkdir_p(File.dirname(file_name))
+    document.render_file(file_name)
+  end
 end
