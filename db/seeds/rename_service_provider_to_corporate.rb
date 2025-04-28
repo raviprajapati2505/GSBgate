59ee763040edb37fdd59517854dade88828079ea
raviprajapati2505 <raviprajapati2505@gmail.com>
2025-04-28 22:21:29 +0530

    remove trust from certification path statuses seed

--- db/seeds/rename_service_provider_to_corporate.rb
+++ db/seeds/rename_service_provider_to_corporate.rb
@@ -9,3 +9,13 @@ Licence.where(licence_type: "ServiceProviderLicence").find_each do |licence|
   licence.description = licence.description.gsub!(/Service Provider/, "Corporate")
   licence.save
 end
+
+# rename audit log visibility
+AuditLogVisibility.find_by(name:'GSB trust team internal').update(name: "GSB team internal")
+
+# remove trust certification path statues
+
+CertificationPathStatus.where("description LIKE ?", "%trust%").find_each do |status|
+  updated_description = status.description.gsub(/\btrust\b/i, '').squeeze(' ').strip
+  status.update!(description: updated_description)
+end
